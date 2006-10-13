1774e76165c1d1d7c97f8b34a377ca6ade4e493b
Felix Gnass <felix.gnass@riotfamily.org>
2006-10-13 15:46:05 +0000

    Added a base class for role based security policies.

---
+++ riot-hibernate/src/org/riotfamily/riot/hibernate/security/AbstractRoleBasedPolicy.java
@@ -0,0 +1,43 @@
+package org.riotfamily.riot.hibernate.security;
+
+import java.util.HashMap;
+
+import org.riotfamily.riot.editor.EditorDefinition;
+import org.riotfamily.riot.security.policy.AuthorizationPolicy;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public abstract class AbstractRoleBasedPolicy extends HibernateDaoSupport 
+		implements AuthorizationPolicy {
+
+	private int order = Integer.MAX_VALUE - 1;
+	
+	private HashMap roles = new HashMap();
+	
+    public int getOrder() {
+		return this.order;
+	}
+	
+    public void setOrder(int order) {
+		this.order = order;
+	}
+    
+	public final int checkPermission(String subject, String action, 
+			Object object, EditorDefinition editor) {
+		
+		return checkRolePermission(getRole(subject), action, object, editor);
+	}
+	
+	protected String getRole(String userId) {
+		String role = (String) roles.get(userId);
+		if (role == null) {
+			User user = (User) getHibernateTemplate().load(User.class, userId);
+			role = user.getRole();
+			roles.put(userId, role);
+		}
+		return role;
+	}
+	
+	protected abstract int checkRolePermission(String role, String action, 
+			Object object, EditorDefinition editor);
+	
+}
