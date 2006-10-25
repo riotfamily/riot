330cd24817449b1f65f0a97e6c1138157c601367
Felix Gnass <felix.gnass@riotfamily.org>
2006-10-25 13:10:43 +0000

    Minor change: Fixed a typo in JavaDoc comment.

--- common/src/org/riotfamily/common/web/util/IncludeFirstInterceptor.java
+++ common/src/org/riotfamily/common/web/util/IncludeFirstInterceptor.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
- * HandlerInterceptor that allows an included controllers to handle the request
+ * HandlerInterceptor that allows an included controller to handle the request
  * before any other controller is executed. This is useful if an included 
  * controller needs to send a redirect, which would not work under normal 
- * circumstances, since the response would already be commited.
+ * circumstances, since the response would have already been commited.
  * <p>
  * The interceptor looks for a special request parameter which contains the URI
  * of the controller that should handle the request in the fist place. If such
