4575bff1e553f05cdf09321d1ae4d5bc2a3ecbf0
Felix Gnass <felix.gnass@riotfamily.org>
2006-11-06 16:42:37 +0000

    Fixed height calculation of the overlay layer.

--- jslibs/riot/src/viewport.js
+++ jslibs/riot/src/viewport.js
@@ -1,38 +1,53 @@
 var Viewport = {};
 
 Viewport.getInnerHeight = function() {
-	if (self.innerHeight) { return self.innerHeight; }
-	if (document.documentElement && document.documentElement.clientHeight) {
-		return document.documentElement.clientHeight;
-	}
-	if (document.body) { return document.body.clientHeight; }
+	if (!w) var w = window;
+	if (w.innerHeight) {
+		// all except Explorer
+		return w.innerHeight;
+	} 
+	var d = w.document;
+	if (d.documentElement && d.documentElement.clientHeight) { 
+		// Explorer 6 Strict Mode
+		return d.documentElement.clientHeight;
+	} 
+	// other Explorers
+	return d.body.clientHeight;
 }
 
 Viewport.getInnerWidth = function(w) {
 	if (!w) var w = window;
 	if (w.innerWidth) { 
+		// all except Explorer
 		return w.innerWidth;
 	}
 	var d = w.document;
 	if (d.documentElement && d.documentElement.clientWidth) {
+		// Explorer 6 Strict Mode
 		return d.documentElement.clientWidth;
 	}
+	// other Explorers
 	return d.body.clientWidth; 
 }
 
 Viewport.getPageHeight = function(w) {
-	if (!w) var w = window;
-	return Math.max(Viewport.getBodyHeight(w), Viewport.getInnerHeight(w));
+	var bodyHeight = Viewport.getBodyHeight(w);
+	var windowHeight = Viewport.getInnerHeight(w);
+	return bodyHeight < windowHeight ? windowHeight : bodyHeight;
 }
 
 Viewport.getBodyHeight = function(w) {
-	var d = w ? w.document : document;
-	if (d.documentElement && d.documentElement.clientHeight) {
-		return d.documentElement.clientHeight;
+	if (!w) var w = window;
+	var d = w.document;
+	if (w.innerHeight && w.scrollMaxY) {	
+		return w.innerHeight + w.scrollMaxY;
 	} 
-	else {
-		return d.body.clientHeight;
-	}
+	if (d.body.scrollHeight > d.body.offsetHeight) { 
+		// all but Explorer Mac
+		return d.body.scrollHeight;
+	} 
+	// Explorer Mac... would also work in Explorer 6 Strict, Mozilla and Safari
+	return d.body.offsetHeight;
 }
 
 Viewport.getScrollTop = function() {
