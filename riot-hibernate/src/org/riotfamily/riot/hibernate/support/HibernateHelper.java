1ce7d8f2e6206a1b0d32dc47ab33fe7c43a0e462
Felix Gnass <felix.gnass@riotfamily.org>
2007-04-27 16:33:40 +0000

    Added the HibernateHelper class that can be used in DAOs that use the plain Hibernate 3 API but want to throw Spring DataAccessExceptions.

---
+++ riot-hibernate/src/org/riotfamily/riot/hibernate/support/HibernateHelper.java
@@ -0,0 +1,280 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Riot.
+ *
+ * The Initial Developer of the Original Code is
+ * Neteye GmbH.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Felix Gnass [fgnass at neteye dot de]
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.riotfamily.riot.hibernate.support;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
+
+/**
+ * Class that provides helper methods to work with contextual sessions.
+ * All HibernateExceptions are caught and converted to DataAccessExceptions.
+ *
+ * @author Felix Gnass [fgnass at neteye dot de]
+ * @since 6.5
+ */
+public class HibernateHelper extends HibernateSupport {
+
+	private String defaultCacheRegion = null;
+
+	public HibernateHelper(SessionFactory sessionFactory) {
+		this(sessionFactory, null);
+	}
+
+	public HibernateHelper(SessionFactory sessionFactory,
+			String defaultCacheReqion) {
+
+		setSessionFactory(sessionFactory);
+		this.defaultCacheRegion = defaultCacheReqion;
+	}
+
+	/**
+	 * Returns the current session.
+	 * @see SessionFactory#getCurrentSession()
+	 */
+	public Session getSession() {
+		try {
+			return super.getSession();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Creates a Criteria for the given Class.
+	 */
+	public Criteria createCriteria(Class clazz) {
+		try {
+			return super.createCriteria(clazz);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Creates a cacheable Criteria for the given Class, using the helper's
+	 * defaultCacheRegion.
+	 */
+	public Criteria createCacheableCriteria(Class clazz) {
+		return createCacheableCriteria(defaultCacheRegion, clazz);
+	}
+
+	/**
+	 * Creates a cacheable Criteria for the given Class, using the specified
+	 * cache region.
+	 */
+	public Criteria createCacheableCriteria(String cacheRegion, Class clazz) {
+		try {
+			Criteria c = super.createCriteria(clazz);
+			c.setCacheRegion(cacheRegion);
+			c.setCacheable(true);
+			return c;
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Creates Query for the given HQL.
+	 */
+	public Query createQuery(String hql) {
+		try {
+			return super.createQuery(hql);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Creates a cacheable Query for the given HQL, using the helper's
+	 * defaultCacheRegion.
+	 */
+	public Query createCacheableQuery(String hql) {
+		return createCacheableQuery(defaultCacheRegion, hql);
+	}
+
+	/**
+	 * Creates a cacheable Query for the given HQL, using the specified
+	 * cache region.
+	 */
+	public Query createCacheableQuery(String cacheRegion, String hql) {
+		try {
+			Query query = super.createQuery(hql);
+			query.setCacheRegion(cacheRegion);
+			query.setCacheable(true);
+			return query;
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Returns the persistent instance of the given Class, assuming that the
+	 * instance exists.
+	 * @see Session#load(Class, Serializable)
+	 */
+	public Object load(Class clazz, Serializable id) throws DataAccessException {
+		try {
+			return getSession().load(clazz, id);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Returns the persistent instance of the given Class, or <code>null</code>
+	 * if no such instance exists.
+	 * @see Session#get(Class, Serializable)
+	 */
+	public Object get(Class clazz, Serializable id) throws DataAccessException {
+		try {
+			return getSession().get(clazz, id);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Persists the given transient instance.
+	 * @see Session#save(Object)
+	 */
+	public Serializable save(Object object) throws DataAccessException {
+		try {
+			return getSession().save(object);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Updates the given persistent instance.
+	 * @see Session#update(Object)
+	 */
+	public void update(Object object) throws DataAccessException {
+		try {
+			getSession().update(object);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Deletes the given persistent instance.
+	 * @see Session#delete(Object)
+	 */
+	public void delete(Object object) throws DataAccessException {
+		try {
+			getSession().delete(object);
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	/**
+	 * Forces the current session to flush.
+	 * @see Session#flush()
+	 */
+	public void flush() throws DataAccessException {
+		try {
+			getSession().flush();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	public List list(Query query) throws DataAccessException {
+		try {
+			return query.list();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	public Object uniqueResult(Query query) throws DataAccessException {
+		try {
+			return query.uniqueResult();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	public List list(Criteria c) throws DataAccessException {
+		try {
+			return c.list();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	public Object uniqueResult(Criteria c) throws DataAccessException {
+		try {
+			return c.uniqueResult();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+	public void setParameter(Query query, String name, Object val) {
+		if (val != null) {
+			try {
+				query.setParameter(name, val);
+			}
+			catch (HibernateException e) {
+				throw SessionFactoryUtils.convertHibernateAccessException(e);
+			}
+		}
+	}
+
+	public int executeUpdate(Query query) {
+		try {
+			return query.executeUpdate();
+		}
+		catch (HibernateException e) {
+			throw SessionFactoryUtils.convertHibernateAccessException(e);
+		}
+	}
+
+}
--- riot-hibernate/src/org/riotfamily/riot/hibernate/support/HibernateUtils.java
+++ riot-hibernate/src/org/riotfamily/riot/hibernate/support/HibernateUtils.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.hibernate.support;
 
@@ -28,32 +28,34 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.hibernate.Criteria;
 import org.hibernate.EntityMode;
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Expression;
 import org.hibernate.metadata.ClassMetadata;
 import org.riotfamily.common.beans.PropertyUtils;
 import org.springframework.util.StringUtils;
 
 public final class HibernateUtils {
-	
+
 	private HibernateUtils() {
 	}
-	
+
 	public static Object get(Session session, Class beanClass, String id) {
 		SessionFactory sf = session.getSessionFactory();
 		Serializable serialId = convertId(beanClass, id, sf);
 		return session.get(beanClass, serialId);
 	}
-	
-	public static Serializable convertId(Class beanClass, String id, 
+
+	public static Serializable convertId(Class beanClass, String id,
 			SessionFactory sessionFactory) {
-		
+
 		Class identifierClass = sessionFactory.getClassMetadata(beanClass)
 				.getIdentifierType().getReturnedClass();
-		
+
 		return (Serializable) PropertyUtils.convert(id, identifierClass);
 	}
 
@@ -62,15 +64,15 @@ public final class HibernateUtils {
 		ClassMetadata metadata = sessionFactory.getClassMetadata(clazz);
 		return metadata.getIdentifier(bean, EntityMode.POJO).toString();
 	}
-	
+
 	/**
 	 * Returns a HQL term that can be used within a where-clause to perform
 	 * a query-by-example.
 	 * @since 6.4
 	 */
-	public static String getExampleWhereClause(Object example, String alias, 
+	public static String getExampleWhereClause(Object example, String alias,
 			String[] propertyNames) {
-		
+
 		if (example == null || propertyNames == null) {
 			return null;
 		}
@@ -106,7 +108,7 @@ public final class HibernateUtils {
 		}
 		return hql.length() > 0 ? hql.toString() : null;
 	}
-	
+
 	/**
 	 * Sets collection values as individual query parameters. Use this method
 	 * together with {@link #getExampleWhereClause(Object, String, String[])}
@@ -114,15 +116,15 @@ public final class HibernateUtils {
 	 * <p>
 	 * The method iterates over the provides names array and inspects the given
 	 * bean (or map). If there's a property (or map entry) of the type
-	 * <code>java.util.Collection</code>, the methods iterates over the 
+	 * <code>java.util.Collection</code>, the methods iterates over the
 	 * collection and sets a query parameter for each item. The name is suffixed
 	 * with an underscore and the item's index.
-	 * 
+	 *
 	 * @since 6.4
 	 */
-	public static void setCollectionValueParams(Query query, 
+	public static void setCollectionValueParams(Query query,
 			String[] names, Object object) {
-		
+
 		for (int i = 0; i < names.length; i++) {
 			String name = names[i];
 			Object value;
@@ -141,17 +143,17 @@ public final class HibernateUtils {
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns a HQL term that can be used within a where-clause to perform
-	 * a search. Example: <code>"(lower(&lt;alias&gt;.&lt;property[0]&gt;) 
+	 * a search. Example: <code>"(lower(&lt;alias&gt;.&lt;property[0]&gt;)
 	 * like :&lt;searchParamName&gt; or lower(&lt;alias&gt;.&lt;property[1]&gt;)
 	 * like :&lt;searchParamName&gt; or ...)"</code>
 	 * @since 6.4
 	 */
-	public static String getSearchWhereClause(String alias, 
+	public static String getSearchWhereClause(String alias,
 			String[] propertyNames, String searchParamName) {
-		
+
 		if (propertyNames == null || propertyNames.length == 0) {
 			return null;
 		}
@@ -167,16 +169,16 @@ public final class HibernateUtils {
 		hql.append(')');
 		return hql.toString();
 	}
-	
+
 	/**
 	 * Appends the given term to the StringBuffer. If the buffer is not empty,
 	 * the provided expression is inserted right before the term (surrounded
 	 * by spaces).
 	 * @since 6.4
 	 */
-	public static StringBuffer appendHql(StringBuffer hql, 
+	public static StringBuffer appendHql(StringBuffer hql,
 			String expression, String term) {
-		
+
 		if (StringUtils.hasText(term)) {
 			if (expression != null && hql.length() > 0) {
 				hql.append(' ').append(expression).append(' ');
@@ -184,5 +186,14 @@ public final class HibernateUtils {
 			hql.append(term);
 		}
 		return hql;
+	}
+
+	public static void addEqOrNull(Criteria c, String name, Object val) {
+		if (val != null) {
+			c.add(Expression.eq(name, val));
+		}
+		else {
+			c.add(Expression.isNull(name));
+		}
 	}
 }
