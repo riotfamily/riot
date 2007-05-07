42bd6b4573ac8eb9645870002c2528c362a948ec
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-07 18:04:59 +0000

    Removed the browserinfo.js script as we now use Prototype.Browser instead.

--- jslibs/riot/src/browserinfo.js
+++
@@ -1,9 +0,0 @@
-var browserInfo = new function() {
-	var agt = navigator.userAgent.toLowerCase();
-	this.safari = agt.indexOf('safari') != -1 && agt.indexOf('mac') != -1;
-	this.konqueror = agt.indexOf('konqueror') != -1;
-	this.khtml = this.safari || this.konqueror;
-	this.gecko = !this.khtml && navigator.product && navigator.product.toLowerCase() == 'gecko';
-	this.opera = agt.indexOf('opera') != -1;
-	this.ie = navigator.appVersion.toLowerCase().indexOf('msie') != -1 && !this.opera && !this.khtml;
-}
