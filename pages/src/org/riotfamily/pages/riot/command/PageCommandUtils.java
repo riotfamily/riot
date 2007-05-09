8d2ee1f5d44f373acaa45f9e9afe8b101ee35d7a
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-09 21:30:11 +0000

    Factored out the PageCommandUtils from the various page commands.

--- pages/src/org/riotfamily/pages/riot/command/PageCommandUtils.java
+++ pages/src/org/riotfamily/pages/riot/command/PageCommandUtils.java
@@ -23,15 +23,44 @@
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.pages.riot.command;
 
+import java.util.Locale;
+
+import org.riotfamily.pages.Page;
 import org.riotfamily.riot.list.command.CommandContext;
-import org.riotfamily.riot.list.command.core.DeleteCommand;
+import org.springframework.util.ObjectUtils;
 
-public class DeletePageCommand extends DeleteCommand {
+/**
+ * @author Felix Gnass [fgnass at neteye dot de]
+ * @since 6.5
+ */
+public final class PageCommandUtils {
 
-	protected boolean isEnabled(CommandContext context, String action) {
-		return super.isEnabled(context, action)
-				&& !PageCommandUtils.isSystemPage(context)
-				&& PageCommandUtils.isTranslated(context);
+	private PageCommandUtils() {
 	}
 
+	public static Page getPage(CommandContext context) {
+		return (Page) context.getBean();
+	}
+
+	public static Locale getParentLocale(CommandContext context) {
+		Object parent = context.getParent();
+		Locale locale = null;
+		if (parent instanceof Page) {
+			locale = ((Page) parent).getLocale();
+		}
+		else if (parent instanceof Locale) {
+			locale = (Locale) parent;
+		}
+		return locale;
+	}
+
+	public static boolean isTranslated(CommandContext context) {
+		Page page = getPage(context);
+		Locale locale = getParentLocale(context);
+		return ObjectUtils.nullSafeEquals(page.getLocale(), locale);
+	}
+
+	public static boolean isSystemPage(CommandContext context) {
+		return getPage(context).getNode().isSystemNode();
+	}
 }
