69fede57c28f4260efd5fe207ac93df03367cfd3
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-09 15:11:57 +0000

    Added an utility method that returns a XML attribute as java.lang.Integer.

--- common/src/org/riotfamily/common/xml/XmlUtils.java
+++ common/src/org/riotfamily/common/xml/XmlUtils.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.xml;
 
@@ -58,21 +58,21 @@ import org.xml.sax.SAXException;
 
 /**
  * Utility class that provides methods to create and populate beans from
- * DOM elements. 
+ * DOM elements.
  */
 public class XmlUtils {
-		
+
 	private static final String PROPERTY_NAME = "name";
-	
+
 	private static final String PROPERTY_VALUE = "value";
-	
+
 	private static final String PROPERTY_REF = "ref";
 
 	public static String getLocalName(Node node) {
 		String name = node.getLocalName();
 		return name != null ? name : node.getNodeName();
 	}
-	
+
 	public static List getChildElements(Element ele) {
 		NodeList nl = ele.getChildNodes();
 		List childEles = new LinkedList();
@@ -84,7 +84,7 @@ public class XmlUtils {
 		}
 		return childEles;
 	}
-		
+
 	public static List getChildElementsByRegex(Element ele, String pattern) {
 		NodeList nl = ele.getChildNodes();
 		List childEles = new LinkedList();
@@ -96,7 +96,7 @@ public class XmlUtils {
 		}
 		return childEles;
 	}
-	
+
 	public static Element getFirstChildElement(Element parent) {
 		Node child = parent.getFirstChild();
 		while (child != null) {
@@ -107,7 +107,7 @@ public class XmlUtils {
 		}
 		return null;
 	}
-		
+
 	public static Element getFirstChildByTagName(Element ele, String tagName) {
 		NodeList nl = ele.getChildNodes();
 		for (int i = 0; i < nl.getLength(); i++) {
@@ -118,7 +118,7 @@ public class XmlUtils {
 		}
 		return null;
 	}
-	
+
 	public static Element getFirstChildByRegex(Element ele, String pattern) {
 		NodeList nl = ele.getChildNodes();
 		for (int i = 0; i < nl.getLength(); i++) {
@@ -129,16 +129,28 @@ public class XmlUtils {
 		}
 		return null;
 	}
-	
+
 	public static String getAttribute(Element ele, String name) {
 		Assert.hasText(name, "An attribute name must be specified");
 		Attr attr = ele.getAttributeNode(name);
 		return attr != null ? attr.getNodeValue() : null;
 	}
-	
-	public static int getIntAttribute(Element ele, String name, 
+
+	public static Integer getIntegerAttribute(Element ele, String name) {
+		String s = getAttribute(ele, name);
+		if (s != null) {
+			try {
+				return new Integer(s);
+			}
+			catch (NumberFormatException e) {
+			}
+		}
+		return null;
+	}
+
+	public static int getIntAttribute(Element ele, String name,
 			int defaultValue) {
-		
+
 		String s = getAttribute(ele, name);
 		if (s != null) {
 			try {
@@ -149,12 +161,12 @@ public class XmlUtils {
 		}
 		return defaultValue;
 	}
-	
+
 	public static boolean getBooleanAttribute(Element ele, String name) {
 		String s = getAttribute(ele, name);
 		return Boolean.valueOf(s).booleanValue();
 	}
-	
+
 	public static void populate(Object bean, NamedNodeMap attributes) {
 		if (attributes != null) {
 			BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
@@ -166,42 +178,42 @@ public class XmlUtils {
 			}
 		}
 	}
-	
-	public static void populate(Object bean, Element element, 
+
+	public static void populate(Object bean, Element element,
 			String[] attributeNames) {
-		
+
 		populate(bean, element, attributeNames, null);
 	}
-	
-	public static void populate(Object bean, Element element, 
+
+	public static void populate(Object bean, Element element,
 			String[] attributeNames, BeanFactory beanFactory) {
-		
+
 		BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
 		for (int i = 0; i < attributeNames.length; i++) {
 			setProperty(beanWrapper, attributeNames[i], element, beanFactory);
 		}
 	}
-	
-	private static void setProperty(BeanWrapper beanWrapper, String attr, 
+
+	private static void setProperty(BeanWrapper beanWrapper, String attr,
 			Element element, BeanFactory beanFactory) {
-		
+
 		String property = null;
 		int i = attr.indexOf('=');
 		if (i != -1) {
 			property = attr.substring(0, i);
 			attr = attr.substring(i + 1);
 		}
-		
+
 		boolean beanRef = attr.charAt(0) == '@';
 		if (beanRef) {
 			attr = attr.substring(1);
 		}
-		
+
 		if (property == null) {
 			property = FormatUtils.xmlToCamelCase(attr);
 		}
-		
-		String value = element.getAttribute(attr); 
+
+		String value = element.getAttribute(attr);
 		if (value.length() > 0) {
 			if (beanRef) {
 				Object object = beanFactory.getBean(value);
@@ -212,10 +224,10 @@ public class XmlUtils {
 			}
 		}
 	}
-	
-	public static void populate(Object bean, List elements, 
+
+	public static void populate(Object bean, List elements,
 			BeanFactory beanFactory) {
-		
+
 		BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
 		Iterator it = elements.iterator();
 		while (it.hasNext()) {
@@ -225,17 +237,17 @@ public class XmlUtils {
 			if (value == null) {
 				String beanId = getAttribute(ele, PROPERTY_REF);
 				if (beanId != null) {
-					Assert.notNull(beanFactory, 
+					Assert.notNull(beanFactory,
 							"A BeanFactory must be supplied in order to set " +
 							"properties by reference.");
-							
+
 					value = beanFactory.getBean(beanId);
 				}
 			}
 			beanWrapper.setPropertyValue(name, value);
 		}
 	}
-	
+
 	public static Document createDocument() {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		try {
@@ -243,9 +255,9 @@ public class XmlUtils {
 		}
 		catch (ParserConfigurationException e) {
 			throw new RuntimeException(e);
-		}			
+		}
 	}
-	
+
 	public static void removeNode(Node node) {
 		if (node != null) {
 			Node parent = node.getParentNode();
@@ -254,21 +266,21 @@ public class XmlUtils {
 			}
 		}
 	}
-	
+
 	public static void importAndAppend(Node node, Node parent) {
 		parent.appendChild(parent.getOwnerDocument().importNode(node, true));
 	}
-	
+
 	public static void removeAllChildNodes(Node node) {
 		while (node.hasChildNodes()) {
 			node.removeChild(node.getLastChild());
 		}
 	}
-	
+
 	public static void setTextValue(Element element, String value) {
 		removeAllChildNodes(element);
 		if (value != null) {
-			Node text = element.getOwnerDocument().createTextNode(value); 
+			Node text = element.getOwnerDocument().createTextNode(value);
 			element.appendChild(text);
 		}
 	}
@@ -290,7 +302,7 @@ public class XmlUtils {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public static String serialize(Node node) {
 		try {
 			TransformerFactory tf = TransformerFactory.newInstance();
@@ -306,5 +318,5 @@ public class XmlUtils {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 }
