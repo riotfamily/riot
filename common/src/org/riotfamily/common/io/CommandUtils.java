df9cb6d2a6d3c517d0c1bb95c818cdb8f86ceb1c
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-09 15:11:14 +0000

    Added some additional log output when invoking ImageMagick.

--- common/src/org/riotfamily/common/io/CommandUtils.java
+++ common/src/org/riotfamily/common/io/CommandUtils.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.io;
 
@@ -29,30 +29,34 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.util.StringUtils;
 
 /**
  * Provides utility methods to invoke commands via Runtime.exec().
- * 
+ *
  * @author Felix Gnass [fgnass at neteye dot de]
  */
 public final class CommandUtils {
 
+	private static final Log log = LogFactory.getLog(CommandUtils.class);
+
 	private CommandUtils() {
 	}
-	
+
 	public static String exec(String executable) throws IOException {
 		return exec(new String[] {executable});
 	}
-	
+
 	public static String exec(String executable, String arg) throws IOException {
 		return exec(new String[] {executable, arg});
 	}
-	
+
 	public static String exec(List commandLine) throws IOException {
 		return exec(StringUtils.toStringArray(commandLine));
 	}
-	
+
 	public static String exec(String[] commandLine) throws IOException {
 		Process proc = Runtime.getRuntime().exec(commandLine);
 		try {
@@ -61,14 +65,19 @@ public final class CommandUtils {
 				return capture(proc.getInputStream());
 			}
 			else {
-				throw new IOException(capture(proc.getErrorStream()));
+				String msg = capture(proc.getErrorStream());
+				log.error("Error invoking command '"
+						+ StringUtils.arrayToDelimitedString(commandLine, " ")
+						+ "':\n" + msg);
+
+				throw new IOException(msg);
 			}
 		}
 		catch (InterruptedException e) {
 			throw new IOException();
 		}
 	}
-	
+
 	private static String capture(InputStream in) {
 		StringBuffer buffer = new StringBuffer();
 		try {
--- forms/src/org/riotfamily/forms/element/support/image/ImageMagickCropperFactoryBean.java
+++ forms/src/org/riotfamily/forms/element/support/image/ImageMagickCropperFactoryBean.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.forms.element.support.image;
 
@@ -38,15 +38,16 @@ import org.springframework.beans.factory.config.AbstractFactoryBean;
 public class ImageMagickCropperFactoryBean extends AbstractFactoryBean {
 
 	private static Log log = LogFactory.getLog(ImageMagickCropperFactoryBean.class);
-	
+
 	private String convertCommand = "convert";
-	
+
 	public void setConvertCommand(String convertCommand) {
 		this.convertCommand = convertCommand;
 	}
 
 	protected Object createInstance() throws Exception {
 		try {
+			log.info("Looking for ImageMagick binary: " + convertCommand);
 			String version = CommandUtils.exec(convertCommand, "-version");
 			log.info(version);
 			ImageMagickCropper cropper = new ImageMagickCropper();
@@ -59,7 +60,7 @@ public class ImageMagickCropperFactoryBean extends AbstractFactoryBean {
 			return cropper;
 		}
 		catch (IOException e) {
-			log.warn("ImageMagick not found in PATH - cropping disabled.");
+			log.warn("ImageMagick not found - cropping disabled.");
 		}
 		return null;
 	}
