5cbd394678d3cdead8fe0d0a0b83c4d52277bf67
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-09 21:28:11 +0000

    The CommandContext now has a getParent() method.

--- riot/src/org/riotfamily/riot/list/command/core/AbstractCommand.java
+++ riot/src/org/riotfamily/riot/list/command/core/AbstractCommand.java
@@ -4,30 +4,28 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.list.command.core;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.riotfamily.common.util.FormatUtils;
-import org.riotfamily.riot.editor.EditorDefinitionUtils;
-import org.riotfamily.riot.editor.ListDefinition;
 import org.riotfamily.riot.list.command.Command;
 import org.riotfamily.riot.list.command.CommandContext;
 import org.riotfamily.riot.list.command.CommandState;
@@ -41,44 +39,44 @@ public abstract class AbstractCommand implements Command, BeanNameAware {
 	private static final String COMMAND_NAME_SUFFIX = "Command";
 
 	private final String COMMAND_MESSAGE_PREFIX = "command.";
-	
+
 	protected Log log = LogFactory.getLog(getClass());
-	
+
 	private String id;
-	
+
 	private boolean showOnForm;
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	/**
-	 * Sets the commandId. If no value is set the bean name will be used 
+	 * Sets the commandId. If no value is set the bean name will be used
 	 * by default.
-	 * 
+	 *
 	 * @see #setBeanName(String)
 	 */
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	/**
-	 * Implementation of the 
+	 * Implementation of the
 	 * {@link org.springframework.beans.factory.BeanNameAware BeanNameAware}
 	 * interface. If no command id is explicitly set, the bean name will be
-	 * used instead. Note that if the name ends with the suffix "Command" 
-	 * it will be removed from the id.  
+	 * used instead. Note that if the name ends with the suffix "Command"
+	 * it will be removed from the id.
 	 */
 	public void setBeanName(String beanName) {
 		if (id == null) {
 			if (beanName.endsWith(COMMAND_NAME_SUFFIX)) {
-				beanName = beanName.substring(0, beanName.length() - 
+				beanName = beanName.substring(0, beanName.length() -
 						COMMAND_NAME_SUFFIX.length());
 			}
 			id = beanName;
 		}
 	}
-	
+
 	/**
 	 * Always returns <code>null</code>. Sublasses may override this method
 	 * in order to display a confirmation message before the command is
@@ -110,7 +108,7 @@ public abstract class AbstractCommand implements Command, BeanNameAware {
 		state.setItemStyleClass(getItemStyleClass(context, action));
 		return state;
 	}
-	
+
 	/**
 	 * Returns the command's id. Subclasses may override this method if the
 	 * action depends on the context.
@@ -118,42 +116,42 @@ public abstract class AbstractCommand implements Command, BeanNameAware {
 	protected String getAction(CommandContext context) {
 		return getId();
 	}
-	
+
 	/**
-	 * Returns a label by resolving the message-key 
+	 * Returns a label by resolving the message-key
 	 * <code>command.<i>labelKeySuffix</i></code>, where <i>labelKeySuffix</i>
-	 * is the String returned by {@link #getLabelKeySuffix(CommandContext, String)}. 
+	 * is the String returned by {@link #getLabelKeySuffix(CommandContext, String)}.
 	 */
 	protected String getLabel(CommandContext context, String action) {
 		String key = getLabelKeySuffix(context, action);
 		return context.getMessageResolver().getMessage(
-				COMMAND_MESSAGE_PREFIX + key, null, 
+				COMMAND_MESSAGE_PREFIX + key, null,
 				FormatUtils.camelToTitleCase(key));
 	}
-	
+
 	/**
 	 * Returns the command's action. Subclasses may override this method if the
 	 * label depends on the context.
-	 * 
+	 *
 	 * @see #getLabel(CommandContext, String)
 	 */
 	protected String getLabelKeySuffix(CommandContext context, String action) {
 		return action;
 	}
-	
+
 	/**
 	 * Returns the CSS class that is assigned to command's HTML element and
-	 * therefore defines which icon is displayed. The default implementation 
+	 * therefore defines which icon is displayed. The default implementation
 	 * delegates the call to {@link #getAction(CommandContext)}.
 	 */
 	protected String getStyleClass(CommandContext context, String action) {
 		return getAction(context);
 	}
-	
-	/** 
-	 * Returns a CSS class that is added to the list of class names of the 
-	 * whole item/row. The default implementation always returns 
-	 * <code>null</code>. Subclasses may override this method to highlight 
+
+	/**
+	 * Returns a CSS class that is added to the list of class names of the
+	 * whole item/row. The default implementation always returns
+	 * <code>null</code>. Subclasses may override this method to highlight
 	 * a list item depending on the context.
 	 */
 	protected String getItemStyleClass(CommandContext context, String action) {
@@ -161,29 +159,15 @@ public abstract class AbstractCommand implements Command, BeanNameAware {
 	}
 
 	/**
-	 * Subclasses may inspect the given context to decide whether the 
-	 * command should be enabled. Commands don't need to check the 
+	 * Subclasses may inspect the given context to decide whether the
+	 * command should be enabled. Commands don't need to check the
 	 * {@link org.riotfamily.riot.security.policy.AuthorizationPolicy policy} since
 	 * commands will be automatically disabled if the action returned by
 	 * {@link #getAction(CommandContext) getAction()} is denied.
-	 * The default implementation always returns <code>true</code>. 
+	 * The default implementation always returns <code>true</code>.
 	 */
 	protected boolean isEnabled(CommandContext context, String action) {
 		return true;
 	}
-	
-	/**
-	 * Convenience method that can be used by subclasses to load to parent 
-	 * object.
-	 * @since 6.5
-	 */
-	protected Object loadParent(CommandContext context) {
-		String parentId = context.getParentId();
-		if (parentId != null) {
-			ListDefinition listDef = context.getListDefinition();
-			return EditorDefinitionUtils.loadParent(listDef, parentId);
-		}
-		return null;
-	}
-	
+
 }
--- riot/src/org/riotfamily/riot/list/ui/CommandContextImpl.java
+++ riot/src/org/riotfamily/riot/list/ui/CommandContextImpl.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.list.ui;
 
@@ -28,22 +28,25 @@ import javax.servlet.http.HttpServletRequest;
 import org.riotfamily.common.i18n.MessageResolver;
 import org.riotfamily.riot.dao.ListParams;
 import org.riotfamily.riot.dao.RiotDao;
+import org.riotfamily.riot.editor.EditorDefinitionUtils;
 import org.riotfamily.riot.editor.ListDefinition;
 import org.riotfamily.riot.list.ListConfig;
 import org.riotfamily.riot.list.command.CommandContext;
 
 class CommandContextImpl implements CommandContext {
-	
+
 	private ListSession session;
 
 	private ListItem item;
-	
+
 	private int itemsTotal;
-	
+
 	private Object bean;
-	
+
+	private Object parent;
+
 	private HttpServletRequest request;
-	
+
 	public CommandContextImpl(ListSession session, HttpServletRequest request) {
 		this.session = session;
 		this.request = request;
@@ -56,22 +59,30 @@ class CommandContextImpl implements CommandContext {
 	public Class getBeanClass() {
 		return session.getBeanClass();
 	}
-	
+
 	public RiotDao getDao() {
 		return session.getListDefinition().getListConfig().getDao();
 	}
-	
+
 	public Object getBean() {
 		if (bean == null && getObjectId() != null) {
 			bean = session.loadBean(getObjectId());
 		}
 		return bean;
 	}
-	
+
 	public void setBean(Object bean) {
 		this.bean = bean;
 	}
-	
+
+	public Object getParent() {
+		if (parent == null && getParentId() != null) {
+			ListDefinition listDef = getListDefinition();
+			parent = EditorDefinitionUtils.loadParent(listDef, getParentId());
+		}
+		return parent;
+	}
+
 	public void setItemsTotal(int itemsTotal) {
 		this.itemsTotal = itemsTotal;
 	}
@@ -87,7 +98,7 @@ class CommandContextImpl implements CommandContext {
 	public ListConfig getListConfig() {
 		return getListDefinition().getListConfig();
 	}
-	
+
 	public MessageResolver getMessageResolver() {
 		return session.getMessageResolver();
 	}
