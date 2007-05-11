d6be451daeb9d2e0c749b2297c48947194b87f77
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-11 18:46:52 +0000

    Update to DWR 2.0.1.

--- common/src/org/riotfamily/common/web/dwr/DwrController.java
+++ common/src/org/riotfamily/common/web/dwr/DwrController.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.dwr;
 
@@ -47,65 +47,65 @@ import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class DwrController extends AbstractController 
-		implements BeanNameAware, InitializingBean, BeanFactoryAware, 
+public class DwrController extends AbstractController
+		implements BeanNameAware, InitializingBean, BeanFactoryAware,
         ServletContextAware {
 
     private String beanName;
-     
+
 	private String mapping;
-	
+
     private List configurators;
-    
+
     private boolean includeDefaultConfig = true;
-    
+
     private ServletConfig servletConfig;
-    
+
     private Map parameters;
-    
+
     private SpringContainer container;
-    
+
     protected WebContextBuilder webContextBuilder;
-    
+
 	public void setMapping(String mapping) {
 		this.mapping = mapping;
 	}
-	
+
 	public void setBeanName(String beanName) {
 		this.beanName = beanName;
 		if (mapping == null) {
 			mapping = beanName;
 		}
 	}
-    
+
     /**
      * Is called by the Spring container to set the bean factory.
-     * This bean factory is then used to obtain the global DWR configuration 
-     * from. This global configuration is optional as DWR will provide defaults 
+     * This bean factory is then used to obtain the global DWR configuration
+     * from. This global configuration is optional as DWR will provide defaults
      * where possible.
      */
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         container = new SpringContainer();
         container.setBeanFactory(beanFactory);
     }
-    
+
     /**
-     * Sets the configurators to apply to this controller. The configurators 
+     * Sets the configurators to apply to this controller. The configurators
      * are used to set up DWR correctly.
      */
     public void setConfigurators(List configurators) {
         this.configurators = configurators;
     }
-    
+
     /**
 	 * Sets parameters just like the init-parameters of the DwrServlet.
 	 */
 	public void setParameters(Map parameters) {
 		this.parameters = parameters;
 	}
-    
+
     /**
-     * Sets whether the default DWR configuration should be included 
+     * Sets whether the default DWR configuration should be included
      * (default is <code>true</code>).
      * <p>
      * This default configuration contains all build-in creators and converters.
@@ -114,12 +114,12 @@ public class DwrController extends AbstractController
     public void setIncludeDefaultConfig(boolean includeDefaultConfig) {
         this.includeDefaultConfig = includeDefaultConfig;
     }
-	
+
     /**
      * Is called by the Spring container after all properties have been set.
-     * This method actually makes sure the container is correctly initialized 
+     * This method actually makes sure the container is correctly initialized
      * and all configurators are processed.
-     * 
+     *
      * @throws Exception in case setting up fails
      */
     public void afterPropertiesSet() throws Exception {
@@ -128,11 +128,11 @@ public class DwrController extends AbstractController
         servletConfig = new FakeServletConfig(beanName, servletContext, parameters);
 
         try {
-            ContainerUtil.setupDefaults(container);
+            ContainerUtil.setupDefaults(container, servletConfig);
             ContainerUtil.setupFromServletConfig(container, servletConfig);
             container.setupFinished();
 
-            webContextBuilder = StartupUtil.initWebContext(servletConfig, servletContext, null, container);
+            webContextBuilder = StartupUtil.initWebContext(servletConfig, servletContext, container);
             StartupUtil.initServerContext(servletConfig, servletContext, container);
 
             if (includeDefaultConfig) {
@@ -150,23 +150,23 @@ public class DwrController extends AbstractController
     /**
      * Handles all request to this controller.
      * <p>
-     * It delegates to the <code>UrlProcessor</code> and also takes care of 
+     * It delegates to the <code>UrlProcessor</code> and also takes care of
      * setting and unsetting of the current <code>WebContext</code>.
      */
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, 
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
             HttpServletResponse response) throws Exception  {
 
         HttpServletRequest shiftedRequest = new PathShiftingRequestWrapper(
                 request, getPathOffset(request));
-        
+
         try {
             // Set up the web context and delegate to the processor
-            webContextBuilder.set(shiftedRequest, response, servletConfig, 
+            webContextBuilder.set(shiftedRequest, response, servletConfig,
             		getServletContext(), container);
 
-            UrlProcessor processor = (UrlProcessor) 
+            UrlProcessor processor = (UrlProcessor)
             		container.getBean(UrlProcessor.class.getName());
-            
+
             processor.handle(shiftedRequest, response);
         }
         finally {
@@ -175,7 +175,7 @@ public class DwrController extends AbstractController
 
         return null;
     }
-	
+
 	protected int getPathOffset(HttpServletRequest request) {
 		String pathInfo = request.getPathInfo();
 		int i = pathInfo.indexOf(mapping);
--- common/src/org/riotfamily/common/web/dwr/SpringConfigurator.java
+++ common/src/org/riotfamily/common/web/dwr/SpringConfigurator.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.dwr;
 
@@ -43,13 +43,13 @@ import org.springframework.util.StringUtils;
 public class SpringConfigurator implements Configurator {
 
 	private Map serviceBeans;
-	
+
 	private Class[] serviceInterfaces;
-	
+
 	private Properties converterTypes;
-	
+
 	private String signatures;
-	
+
 	/**
 	 * Sets a map of beans to be exported keyed by their script name.
 	 * @param serviceBeans Map of beans to export
@@ -60,7 +60,7 @@ public class SpringConfigurator implements Configurator {
 
 	/**
 	 * Sets the interfaces to be exported. This is a convenient way
-	 * to control which methods should be exposed to the client. This is 
+	 * to control which methods should be exposed to the client. This is
 	 * especially useful when your service beans are AOP proxies.
 	 * If no interfaces are configured only the default access rules apply.
 	 * @param serviceInterfaces Interfaces to export
@@ -68,7 +68,7 @@ public class SpringConfigurator implements Configurator {
 	public void setServiceInterfaces(Class[] serviceInterfaces) {
 		this.serviceInterfaces = serviceInterfaces;
 	}
-	
+
 	public void setConverterTypes(Properties converterTypes) {
 		this.converterTypes = converterTypes;
 	}
@@ -76,11 +76,11 @@ public class SpringConfigurator implements Configurator {
 	public void setSignatures(String signatures) {
 		this.signatures = signatures;
 	}
-	
+
 	public void configure(Container container) {
-		ConverterManager converterManager = (ConverterManager) 
+		ConverterManager converterManager = (ConverterManager)
 				container.getBean(ConverterManager.class.getName());
-		        
+
         if (converterTypes != null) {
 			Iterator it = converterTypes.entrySet().iterator();
 			while (it.hasNext()) {
@@ -101,11 +101,11 @@ public class SpringConfigurator implements Configurator {
 				}
 			}
 		}
-        
-        CreatorManager creatorManager = (CreatorManager) 
+
+        CreatorManager creatorManager = (CreatorManager)
         		container.getBean(CreatorManager.class.getName());
-        
-        
+
+
         if (serviceBeans != null) {
 			Iterator it = serviceBeans.entrySet().iterator();
 			while (it.hasNext()) {
@@ -114,23 +114,24 @@ public class SpringConfigurator implements Configurator {
 				BeanCreator creator = new BeanCreator();
 				creator.setScope(Creator.APPLICATION);
 				creator.setBean(entry.getValue());
+				creator.afterPropertiesSet();
 				creatorManager.addCreator(scriptName, creator);
 			}
 		}
-        
+
         if (serviceInterfaces != null) {
         	AccessControl accessControl = (AccessControl) container.getBean(
         		AccessControl.class.getName());
-        	
+
         	for (int i = 0; i < serviceInterfaces.length; i++) {
         		String scriptName = findScriptNameForInterface(serviceInterfaces[i]);
         		Method[] methods = serviceInterfaces[i].getDeclaredMethods();
         		for (int m = 0; m < methods.length; m++) {
-        			accessControl.addIncludeRule(scriptName, methods[m].getName());			
+        			accessControl.addIncludeRule(scriptName, methods[m].getName());
         		}
         	}
         }
-        
+
         if (StringUtils.hasText(signatures)) {
             SignatureParser sigp = new SignatureParser(converterManager, creatorManager);
             sigp.parse(signatures);
@@ -146,7 +147,7 @@ public class SpringConfigurator implements Configurator {
 		while (it.hasNext()) {
 			Map.Entry entry = (Map.Entry) it.next();
 			if (serviceInterface.isInstance(entry.getValue())) {
-				return (String) entry.getKey(); 
+				return (String) entry.getKey();
 			}
 		}
 		throw new BeanCreationException("No serviceBean found that implements "
