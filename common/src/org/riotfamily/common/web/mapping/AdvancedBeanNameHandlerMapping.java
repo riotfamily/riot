78cfd04aa84ed001afce87d94e4cfa40466a5464
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-22 16:50:17 +0000

    The $ character is now escaped.

--- common/src/org/riotfamily/common/web/mapping/AdvancedBeanNameHandlerMapping.java
+++ common/src/org/riotfamily/common/web/mapping/AdvancedBeanNameHandlerMapping.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.mapping;
 
@@ -52,23 +52,23 @@ import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapt
 
 /**
  * HandlerMapping that works like Spring's BeanNameUrlHandlerMapping and
- * can expose parts of the matched URL as request attributes. 
- * <p>The handler name <code>/foo/bar/@{some}/@{value}</code> would be 
+ * can expose parts of the matched URL as request attributes.
+ * <p>The handler name <code>/foo/bar/@{some}/@{value}</code> would be
  * equivalent to <code>/foo/bar/&#42;/&#42;</code>, the last two wildcards
  * would be exposed as attributes "some" and "value".
  */
 public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 		implements HandlerMapping, UrlMapping, Ordered {
-    
-	private static final Pattern ATTRIBUTE_NAME_PATTERN = 
+
+	private static final Pattern ATTRIBUTE_NAME_PATTERN =
 			Pattern.compile("@\\{(.*?)(\\*?)\\}");
-	
-	private static final Pattern STAR_PATTERN = 
+
+	private static final Pattern STAR_PATTERN =
 			Pattern.compile("\\\\\\*");
-	
-	private static final Pattern DOUBLE_STAR_PATTERN = 
+
+	private static final Pattern DOUBLE_STAR_PATTERN =
 			Pattern.compile("\\\\\\*\\\\\\*");
-	
+
 	private int order = Integer.MAX_VALUE;  // default: same as non-Ordered
 
 	private Object defaultHandler;
@@ -78,17 +78,17 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 	private PathMatcher pathMatcher = new AntPathMatcher();
 
 	private final Map handlerMap = new HashMap();
-	    
+
     private HashMap patternsByAntPath = new HashMap();
-    
+
     private HashMap patternsByBeanName = new HashMap();
 
     private String servletMappingPrefix;
-    
+
     private String servletMappingSuffix;
-    
+
     private boolean stripServletMapping = true;
-    
+
 	public void setStripServletMapping(boolean stripServletMapping) {
 		this.stripServletMapping = stripServletMapping;
 	}
@@ -100,7 +100,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
     public void setServletMappingSuffix(String suffix) {
         this.servletMappingSuffix = suffix;
     }
-    
+
 	public final void setOrder(int order) {
 		this.order = order;
 	}
@@ -186,23 +186,23 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 	 * @param request current HTTP request
 	 * @return the looked up handler instance, or the default handler
 	 */
-	public final HandlerExecutionChain getHandler(HttpServletRequest request) 
+	public final HandlerExecutionChain getHandler(HttpServletRequest request)
 			throws Exception {
-		
+
 		String lookupPath = getLookupPath(request);
 		if (logger.isDebugEnabled()) {
 			logger.debug("Looking up handler for [" + lookupPath + "]");
 		}
-		
+
 		Object handler = lookupHandler(lookupPath, request);
-		
+
 		if (handler == null) {
 			handler = this.defaultHandler;
 		}
 		if (handler == null) {
 			return null;
 		}
-		
+
 		// bean name of resolved handler?
 		if (handler instanceof String) {
 			String handlerName = (String) handler;
@@ -213,7 +213,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 		}
 		return new HandlerExecutionChain(handler, this.interceptors);
 	}
-	
+
 	/**
 	 * Look up a handler instance for the given URL path.
 	 * <p>Supports direct matches, e.g. a registered "/test" matches "/test",
@@ -236,7 +236,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 				String registeredPath = (String) it.next();
 				if (this.pathMatcher.match(registeredPath, urlPath) &&
 						(bestPathMatch == null || bestPathMatch.length() <= registeredPath.length())) {
-					
+
 					handler = this.handlerMap.get(registeredPath);
 					bestPathMatch = registeredPath;
 				}
@@ -284,7 +284,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 		else {
 			this.handlerMap.put(urlPath, handler);
 			if (logger.isDebugEnabled()) {
-				logger.debug("Mapped URL path [" + urlPath 
+				logger.debug("Mapped URL path [" + urlPath
 						+ "] onto handler [" + handler + "]");
 			}
 		}
@@ -296,7 +296,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 	protected void exposePathWithinMapping(String pathWithinMapping, HttpServletRequest request) {
 		request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, pathWithinMapping);
 	}
-    
+
 	/**
 	 * <strong>Copied from BeanNameUrlHandlerMapping</strong>
 	 */
@@ -329,7 +329,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 			}
 		}
 	}
-	
+
 	/**
 	 * Check name and aliases of the given bean for URLs,
 	 * detected by starting with "/".
@@ -348,19 +348,19 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 		}
 		return StringUtils.toStringArray(urls);
 	}
-    
+
 	protected String convertToAntPattern(String urlPattern) {
 		return ATTRIBUTE_NAME_PATTERN.matcher(urlPattern).replaceAll("*$2");
 	}
-		
-	protected void exposeAttributes(String antPattern, String urlPath, 
+
+	protected void exposeAttributes(String antPattern, String urlPath,
 			HttpServletRequest request) {
-		
+
 		AttributePattern pattern = (AttributePattern) patternsByAntPath.get(antPattern);
 		pattern.expose(urlPath, request);
-		
+
 	}
-	
+
 	public String getUrl(String beanName, Map attributes) {
 		List patterns = (List) patternsByBeanName.get(beanName);
 		Iterator it = patterns.iterator();
@@ -380,37 +380,37 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 		//TODO Throw RuntimeException ...
 		return null;
 	}
-	
+
 	private static class AttributePattern {
-		
+
 		private String attributePattern;
-		
+
 		private Pattern pattern;
-		
+
 		private ArrayList attributeNames;
-		
+
 		public AttributePattern(String attributePattern) {
 			this.attributePattern = attributePattern;
-			
+
 			attributeNames = new ArrayList();
 			Matcher m = ATTRIBUTE_NAME_PATTERN.matcher(attributePattern);
 			while (m.find()) {
 				attributeNames.add(m.group(1));
 			}
-			
+
 			pattern = Pattern.compile(convertAttributePatternToRegex(attributePattern));
 		}
-		
+
 		// Example pattern: /resources/*/@{resource*}
 		private String convertAttributePatternToRegex(final String antPattern) {
-			String regex = FormatUtils.escapeChars(antPattern, "()", '\\'); // ... just in case 
+			String regex = FormatUtils.escapeChars(antPattern, "()", '\\'); // ... just in case
 			regex = ATTRIBUTE_NAME_PATTERN.matcher(antPattern).replaceAll("(*$2)"); // /resources/*/(**)
-			regex = "^" + FormatUtils.escapeChars(regex, ".+*?{^$", '\\') + "$"; // ^/resources/\*/(\*\*)$ 
+			regex = "^" + FormatUtils.escapeChars(regex, ".+*?{^$", '\\') + "$"; // ^/resources/\*/(\*\*)$
 			regex = DOUBLE_STAR_PATTERN.matcher(regex).replaceAll(".*?"); // ^/resources/\*/(.*?)$
 			regex = STAR_PATTERN.matcher(regex).replaceAll("[^/]*"); // ^/resources/[^/]*/.*?$
 			return regex;
 		}
-				
+
 		public void expose(String urlPath, HttpServletRequest request) {
 			Matcher m = pattern.matcher(urlPath);
 			Assert.isTrue(m.matches());
@@ -418,7 +418,7 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 				request.setAttribute((String) attributeNames.get(i), m.group(i + 1));
 			}
 		}
-		
+
 		public boolean matches(Map attributes) {
 			if (attributes != null) {
 				Collection names = attributes.keySet();
@@ -429,18 +429,22 @@ public class AdvancedBeanNameHandlerMapping extends WebApplicationObjectSupport
 				return attributeNames.isEmpty();
 			}
 		}
-		
+
 		public StringBuffer buildUrl(Map attributes) {
 			StringBuffer url = new StringBuffer();
 			Matcher m = ATTRIBUTE_NAME_PATTERN.matcher(attributePattern);
 			while (m.find()) {
 				String name = m.group(1);
 				Object value = attributes.get(name);
-				m.appendReplacement(url, value != null ? value.toString() : "");
+				String replacement = value != null
+						? StringUtils.replace(value.toString(), "$", "\\$")
+						: "";
+
+				m.appendReplacement(url, replacement);
 			}
 			m.appendTail(url);
 			return url;
 		}
 	}
-	
+
 }
