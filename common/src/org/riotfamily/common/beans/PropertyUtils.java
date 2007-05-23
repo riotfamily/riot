1932101c8a360b387eb57a401c136572d7600a01
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-23 14:08:06 +0000

    getDeclaringClass() now calls ClassUtils.getUserClass() to handle CGLib proxies correctly.

--- common/src/org/riotfamily/common/beans/PropertyUtils.java
+++ common/src/org/riotfamily/common/beans/PropertyUtils.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.beans;
 
@@ -47,15 +47,15 @@ import org.springframework.util.StringUtils;
  */
 public final class PropertyUtils {
 
-	private static DefaultPropertyEditorRegistry registry = 
+	private static DefaultPropertyEditorRegistry registry =
 			new DefaultPropertyEditorRegistry();
 
 	private static Pattern expressionPattern = Pattern.compile(
 			"\\$\\{(.*?)\\}");
-	
+
 	private PropertyUtils() {
 	}
-	
+
 	public static Object getProperty(Object bean, String name) {
 		if (bean == null) {
 			return null;
@@ -63,22 +63,22 @@ public final class PropertyUtils {
 		ProtectedBeanWrapper wrapper = new ProtectedBeanWrapper(bean);
 		return wrapper.getPropertyValue(name);
 	}
-	
+
 	public static String getPropertyAsString(Object bean, String name) {
 		return convertToString(getProperty(bean, name));
 	}
-	
+
 	public static void setProperty(Object bean, String name, Object value) {
 		ProtectedBeanWrapper wrapper = new ProtectedBeanWrapper(bean);
 		wrapper.setPropertyValue(name, value);
 	}
-	
+
 	public static void setPropertyAsString(Object bean, String name, String s) {
 		Class type = getPropertyType(bean.getClass(), name);
 		Object value = convert(s, type);
 		setProperty(bean, name, value);
 	}
-	
+
 	/**
 	 * Returns a Map containing the bean's properties.
 	 * @since 6.4
@@ -87,12 +87,12 @@ public final class PropertyUtils {
 		PropertyDescriptor[] pd = BeanUtils.getPropertyDescriptors(bean.getClass());
 		HashMap properties = new HashMap();
 		for (int i = 0; i < pd.length; i++) {
-			Object value = ReflectionUtils.invokeMethod(pd[i].getReadMethod(), bean); 
+			Object value = ReflectionUtils.invokeMethod(pd[i].getReadMethod(), bean);
 			properties.put(pd[i].getName(), value);
 		}
 		return properties;
 	}
-	
+
 	/**
 	 * Returns a Map containing the bean's properties.
 	 * @since 6.4
@@ -105,7 +105,7 @@ public final class PropertyUtils {
 		}
 		return properties;
 	}
-	
+
 	/**
 	 * @since 6.4
 	 */
@@ -120,7 +120,7 @@ public final class PropertyUtils {
 		matcher.appendTail(sb);
 		return sb.toString();
 	}
-	
+
 	public static Object convert(String s, Class targetClass) {
 		if (targetClass.equals(String.class)) {
 			return s;
@@ -132,7 +132,7 @@ public final class PropertyUtils {
 			return pe.getValue();
 		}
 	}
-	
+
 	/**
 	 * @since 6.4
 	 */
@@ -151,7 +151,7 @@ public final class PropertyUtils {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * @deprecated as of Riot 6.4, in favor of Spring's <code>BeanUtils.getPropertyDescriptor</code>.
 	 */
@@ -166,7 +166,7 @@ public final class PropertyUtils {
         Assert.notNull(pd, "Property '" + property + "' not found in class " + clazz);
         return pd.getPropertyType();
     }
-    
+
     /**
 	 * @since 6.4
 	 */
@@ -178,7 +178,7 @@ public final class PropertyUtils {
 		}
 		return readMethod;
 	}
-	
+
 	/**
 	 * @since 6.4
 	 */
@@ -186,39 +186,39 @@ public final class PropertyUtils {
 		String methodName = "set" + StringUtils.capitalize(property);
 		Method writeMethod = BeanUtils.findDeclaredMethodWithMinimalParameters(
 				clazz, methodName);
-		
+
 		if (writeMethod != null) {
 			writeMethod.setAccessible(true);
 		}
 		return writeMethod;
 	}
-    
+
     /**
-     * Returns the (super-)class where the given property is declared. 
-     */    
-    public static Class getDeclaringClass(Class clazz, 
+     * Returns the (super-)class where the given property is declared.
+     */
+    public static Class getDeclaringClass(Class clazz,
             String property) {
 
-        PropertyDescriptor[] descriptors = 
+        PropertyDescriptor[] descriptors =
         		BeanUtils.getPropertyDescriptors(clazz);
-        
+
         for (int i = 0; i < descriptors.length; i++) {
             if (descriptors[i].getName().equals(property)) {
                 Method getter = descriptors[i].getReadMethod();
                 if (getter == null) {
-                    return clazz; 
+                    return clazz;
                 }
                 return getter.getDeclaringClass();
             }
         }
-        return clazz;
+        return ClassUtils.getUserClass(clazz);
     }
-	
+
     /**
      * Creates a new instance of the class specified by the given name.
-     * The method uses {@link ClassUtils#forName(String)} and 
-     * {@link BeanUtils#instantiateClass(Class)} internally. 
-     * ClassNotFoundExceptions are caught and re-thrown as 
+     * The method uses {@link ClassUtils#forName(String)} and
+     * {@link BeanUtils#instantiateClass(Class)} internally.
+     * ClassNotFoundExceptions are caught and re-thrown as
      * {@link FatalBeanException}.
      */
     public static Object newInstance(String className) {
@@ -230,14 +230,14 @@ public final class PropertyUtils {
 			throw new FatalBeanException(e.getMessage(), e);
 		}
     }
-    
+
     /**
      * Partitions the given collection by inspecting the specified property
      * of the contained items.
-     *  
+     *
      * @param c The collection to partition
      * @param titleProperty The property to use for grouping
-     * @return A list of {@link ObjectGroup ObjectGroups} 
+     * @return A list of {@link ObjectGroup ObjectGroups}
      */
     public static List partition(Collection c, String titleProperty) {
 		ArrayList groups = new ArrayList();
@@ -246,9 +246,9 @@ public final class PropertyUtils {
 		while (it.hasNext()) {
 			Object item = it.next();
 			Object title = getProperty(item, titleProperty);
-			if (group == null || (title != null 
+			if (group == null || (title != null
 					&& !title.equals(group.getTitle()))) {
-				
+
 				group = new ObjectGroup(title, item);
 				groups.add(group);
 			}
