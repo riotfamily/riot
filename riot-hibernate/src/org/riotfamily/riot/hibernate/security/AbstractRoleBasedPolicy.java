d8b6ad05ae4032456edea08b9f6c795562c86591
Felix Gnass <felix.gnass@riotfamily.org>
2007-05-23 16:28:35 +0000

    Changed the visibility of the getRole() method from protected to private.

--- riot-hibernate/src/org/riotfamily/riot/hibernate/security/AbstractRoleBasedPolicy.java
+++ riot-hibernate/src/org/riotfamily/riot/hibernate/security/AbstractRoleBasedPolicy.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.hibernate.security;
 
@@ -30,35 +30,35 @@ import org.riotfamily.riot.security.LoginManager;
 import org.riotfamily.riot.security.policy.AuthorizationPolicy;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public abstract class AbstractRoleBasedPolicy extends HibernateDaoSupport 
+public abstract class AbstractRoleBasedPolicy extends HibernateDaoSupport
 		implements AuthorizationPolicy {
 
 	private int order = Integer.MAX_VALUE - 1;
-	
+
 	private HashMap roles = new HashMap();
-	
+
     public int getOrder() {
 		return this.order;
 	}
-	
+
     public void setOrder(int order) {
 		this.order = order;
 	}
-    
-	public final int checkPermission(String subject, String action, 
+
+	public final int checkPermission(String subject, String action,
 			Object object, EditorDefinition editor) {
-		
+
 		if (LoginManager.ACTION_LOGIN.equals(action)) {
 			invalidateRole(subject);
 		}
 		return checkRolePermission(getRole(subject), action, object, editor);
 	}
-	
+
 	protected void invalidateRole(String userId) {
 		roles.remove(userId);
 	}
-	
-	protected String getRole(String userId) {
+
+	private String getRole(String userId) {
 		String role = (String) roles.get(userId);
 		if (role == null) {
 			User user = (User) getHibernateTemplate().load(User.class, userId);
@@ -67,8 +67,8 @@ public abstract class AbstractRoleBasedPolicy extends HibernateDaoSupport
 		}
 		return role;
 	}
-	
-	protected abstract int checkRolePermission(String role, String action, 
+
+	protected abstract int checkRolePermission(String role, String action,
 			Object object, EditorDefinition editor);
-	
+
 }
