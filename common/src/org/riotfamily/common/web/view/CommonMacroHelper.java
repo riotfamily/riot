5db410f14864803566ccc06f05b78f12a4f29348
Carsten Wölk <carsten.woelk@riotfamily.org>
2007-06-07 16:27:47 +0000

    Exposed the isExternalUrl method as this is handy to be used via the common macros

--- common/src/org/riotfamily/common/web/view/CommonMacroHelper.java
+++ common/src/org/riotfamily/common/web/view/CommonMacroHelper.java
@@ -1,26 +1,26 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Riot.
- *
- * The Initial Developer of the Original Code is
- * Neteye GmbH.
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Felix Gnass [fgnass at neteye dot de]
- *
- * ***** END LICENSE BLOCK ***** */
+	* Version: MPL 1.1
+	* The contents of this file are subject to the Mozilla Public License Version
+	* 1.1 (the "License"); you may not use this file except in compliance with
+	* the License. You may obtain a copy of the License at
+	* http://www.mozilla.org/MPL/
+	*
+	* Software distributed under the License is distributed on an "AS IS" basis,
+	* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	* for the specific language governing rights and limitations under the
+	* License.
+	*
+	* The Original Code is Riot.
+	*
+	* The Initial Developer of the Original Code is
+	* Neteye GmbH.
+	* Portions created by the Initial Developer are Copyright (C) 2007
+	* the Initial Developer. All Rights Reserved.
+	*
+	* Contributor(s):
+	*   Felix Gnass [fgnass at neteye dot de]
+	*
+	* ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.view;
 
 import java.io.IOException;
@@ -47,9 +47,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
- * @author Felix Gnass [fgnass at neteye dot de]
- * @since 6.5
- */
+	* @author Felix Gnass [fgnass at neteye dot de]
+	* @since 6.5
+	*/
 public class CommonMacroHelper {
 
 	private static final Log log = LogFactory.getLog(CommonMacroHelper.class);
@@ -95,7 +95,7 @@ public class CommonMacroHelper {
  		return uri;
 	}
 
-	protected boolean isExternalUrl(String url) {
+	public boolean isExternalUrl(String url) {
 		try {
 			URI uri = new URI(url);
 			if (!uri.isOpaque()) {
