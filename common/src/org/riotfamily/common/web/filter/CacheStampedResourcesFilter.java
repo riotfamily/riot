09be178dff05eae76ef108ed091f2ecd4afd2fee
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-08 15:06:46 +0000

    The pattern used to detect stamped (versioned) resources can now be configured. Additionally the pattern is not limited to the queryString anymore and can inspect the complete URL.

--- common/src/org/riotfamily/common/web/filter/CacheStampedResourcesFilter.java
+++ common/src/org/riotfamily/common/web/filter/CacheStampedResourcesFilter.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.filter;
 
@@ -34,18 +34,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.riotfamily.common.util.FormatUtils;
+import org.riotfamily.common.web.util.ServletUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 /**
  * Servlet filter that sets an Expires header for request URLs that contain
- * a timestamp.
+ * a timestamp. URLs are considered as 'stamped' if they match the configured
+ * pattern.
  * <p>
- * URLs are considered as 'stamped' if the query string contains only digits.
- * <p>
- * You can use {@link org.riotfamily.common.web.view.freemarker.AddTimestampMethod 
- * this FreeMarker method} to add timestamps to your URLs.
- * The {@link org.riotfamily.website.css.CssTemplateController CssTemplateController}
- * provided by the riot-pages module can be configured to do this automatically.   
+ * The default pattern is <code>(/\\d{14}/)|(\\?[0-9]+$)</code>, this
+ * matches URLs created by the DefaultFileStore and URLs printed via the
+ * <code>common.resource()</code> FreeMarker function.
  * </p>
  * @author Felix Gnass [fgnass at neteye dot de]
  * @since 6.4
@@ -53,32 +52,39 @@ import org.springframework.web.filter.GenericFilterBean;
 public class CacheStampedResourcesFilter extends GenericFilterBean {
 
 	public static final String DEFAULT_EXPIRATION = "1M";
-	
+
+	public static final Pattern DEFAULT_PATTERN =
+			Pattern.compile("(/\\d{14}/)|(\\?[0-9]+$)");
+
 	private static final String EXPIRES_HEADER = "Expires";
 
+	private Pattern stampPattern = DEFAULT_PATTERN;
+
 	private String expiresAfter = DEFAULT_EXPIRATION;
-	
+
 	private long expires;
-	
-	private Pattern stampPattern = Pattern.compile("[0-9]+"); 
-	
+
 	public void setExpiresAfter(String expiresAfter) {
 		this.expiresAfter = expiresAfter;
 	}
-	
+
+	public void setStampPattern(Pattern stampPattern) {
+		this.stampPattern = stampPattern;
+	}
+
 	protected void initFilterBean() throws ServletException {
-		expires = System.currentTimeMillis() 
+		expires = System.currentTimeMillis()
 				+ FormatUtils.parseMillis(expiresAfter);
 	}
-	
-	public final void doFilter(ServletRequest request, ServletResponse response, 
+
+	public final void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
 
-		doFilterInternal((HttpServletRequest) request, 
-				(HttpServletResponse) response, filterChain); 
+		doFilterInternal((HttpServletRequest) request,
+				(HttpServletResponse) response, filterChain);
 	}
-	
-	protected void doFilterInternal(HttpServletRequest request, 
+
+	protected void doFilterInternal(HttpServletRequest request,
 			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
@@ -87,9 +93,9 @@ public class CacheStampedResourcesFilter extends GenericFilterBean {
 		}
 		filterChain.doFilter(request, response);
 	}
-	
+
 	protected boolean isStamped(HttpServletRequest request) {
-		String query = request.getQueryString();
-		return query != null && stampPattern.matcher(query).matches();
+		String url = ServletUtils.getRequestUrlWithQueryString(request);
+		return stampPattern.matcher(url).matches();
 	}
 }
