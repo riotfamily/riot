ccae9e08c5246eb36addb81648b431fa756ad4cd
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-27 11:47:21 +0000

    Fixed a copy-and-paste error: renamed setIncludeRootList to setIncludeRootMap.

--- common/src/org/riotfamily/common/beans/module/ModularMapFactoryBean.java
+++ common/src/org/riotfamily/common/beans/module/ModularMapFactoryBean.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.beans.module;
 
@@ -39,39 +39,39 @@ import org.springframework.context.ApplicationContextAware;
  * Creates a map by merging the entries with the given key from all
  * {@link org.riotfamily.common.beans.module.FactoryBeanModule modules} found in
  * the ApplicationContext.
- * 
+ *
  * @see org.riotfamily.common.beans.module.FactoryBeanModule
  */
-public class ModularMapFactoryBean extends MapFactoryBean 
+public class ModularMapFactoryBean extends MapFactoryBean
 		implements ApplicationContextAware, BeanNameAware {
 
 	private static Log log = LogFactory.getLog(ModularMapFactoryBean.class);
-	
+
 	private String key;
-	
+
 	private boolean includeRootMap;
-	
+
 	private ApplicationContext applicationContext;
-	
+
 	public void setBeanName(String name) {
 		if (key == null) {
 			key = name;
 		}
 	}
-	
+
 	public void setKey(String key) {
 		this.key = key;
 	}
-	
+
 	/**
 	 * Sets whether the root bean defintion (having the key as id) should be
 	 * included in the map. This is useful if not only modules but the
 	 * application itself should be able to add entries to the map.
 	 */
-	public void setIncludeRootList(boolean includeRootMap) {
+	public void setIncludeRootMap(boolean includeRootMap) {
 		this.includeRootMap = includeRootMap;
 	}
-	
+
 	public void setApplicationContext(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
 	}
@@ -80,15 +80,15 @@ public class ModularMapFactoryBean extends MapFactoryBean
 		Map result = (Map) super.createInstance();
 		Collection modules =
 			ModularFactoryBeansUtils.getFactoryBeanModules(applicationContext);
-		
+
 		Iterator it = modules.iterator();
 		while (it.hasNext()) {
 			FactoryBeanModule module = (FactoryBeanModule) it.next();
 			Map moduleMap = module.getMap(key);
 			if (moduleMap != null) {
-				log.info("Adding entries defined by " + module.getName() 
+				log.info("Adding entries defined by " + module.getName()
 						+ " to " + key);
-				
+
 				result.putAll(moduleMap);
 			}
 		}
