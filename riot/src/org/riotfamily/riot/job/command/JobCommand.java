864baab05eb6da0378af1c657b60d4dc5a645cf6
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-27 13:55:57 +0000

    Using static getRuntime() method to get a reference to the RiotRuntime object.

--- riot/src/org/riotfamily/riot/job/command/JobCommand.java
+++ riot/src/org/riotfamily/riot/job/command/JobCommand.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.job.command;
 
@@ -30,21 +30,20 @@ import org.riotfamily.riot.list.command.CommandResult;
 import org.riotfamily.riot.list.command.core.AbstractCommand;
 import org.riotfamily.riot.list.command.result.GotoUrlResult;
 import org.riotfamily.riot.runtime.RiotRuntime;
-import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class JobCommand extends AbstractCommand implements 
+public class JobCommand extends AbstractCommand implements
 		ApplicationContextAware {
 
 	public static final String JOB_STATUS_ACTION = "jobStatus";
-	
+
 	private JobManager jobManager;
-	
+
 	private String jobType;
-	
+
 	private RiotRuntime runtime;
-	
+
 	public void setJobManager(JobManager jobManager) {
 		this.jobManager = jobManager;
 	}
@@ -52,22 +51,21 @@ public class JobCommand extends AbstractCommand implements
 	public void setJobType(String jobType) {
 		this.jobType = jobType;
 	}
-	
+
 	public void setApplicationContext(ApplicationContext context) {
-		runtime = (RiotRuntime) BeanFactoryUtils.beanOfTypeIncludingAncestors(
-				context, RiotRuntime.class);
+		runtime = RiotRuntime.getRuntime(context);
 	}
-	
+
 	public boolean isEnabled(CommandContext context) {
 		return true;
 	}
-	
+
 	public CommandResult execute(CommandContext context) {
-		String objectId = context.getObjectId() != null 
+		String objectId = context.getObjectId() != null
 				? context.getObjectId() : context.getParentId();
-		
+
 		JobDetail detail = jobManager.getOrCreateJob(jobType, objectId);
-		return new GotoUrlResult(context, runtime.getServletPrefix() 
+		return new GotoUrlResult(context, runtime.getServletPrefix()
 				+ "/job?jobId="	+ detail.getId());
 	}
 }
--- riot/src/org/riotfamily/riot/list/export/CsvExportCommand.java
+++ riot/src/org/riotfamily/riot/list/export/CsvExportCommand.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2007
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Jan-Frederic Linde [jfl at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.list.export;
 
@@ -30,19 +30,18 @@ import org.riotfamily.riot.list.command.CommandResult;
 import org.riotfamily.riot.list.command.core.AbstractCommand;
 import org.riotfamily.riot.list.command.result.GotoUrlResult;
 import org.riotfamily.riot.runtime.RiotRuntime;
-import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class CsvExportCommand extends AbstractCommand implements 
+public class CsvExportCommand extends AbstractCommand implements
 		ApplicationContextAware {
-	
+
 	private List properties;
-	
+
 	private String encoding;
-	
+
 	private RiotRuntime runtime;
-	
+
 	public String getEncoding() {
 		return this.encoding;
 	}
@@ -52,18 +51,17 @@ public class CsvExportCommand extends AbstractCommand implements
 	}
 
 	public void setApplicationContext(ApplicationContext context) {
-		runtime = (RiotRuntime) BeanFactoryUtils.beanOfTypeIncludingAncestors(
-				context, RiotRuntime.class);
+		runtime = RiotRuntime.getRuntime(context);
 	}
-	
+
 	public void setProperties(List properties) {
 		this.properties = properties;
 	}
-	
+
 	public List getProperties() {
 		return this.properties;
 	}
-	
+
 	public CommandResult execute(CommandContext context) {
 		StringBuffer url = new StringBuffer();
 		url.append(runtime.getServletPrefix())
