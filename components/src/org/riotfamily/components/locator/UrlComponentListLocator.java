e9aa888a0150e1f8cf1504f86f246dc3dda411d2
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-27 13:48:43 +0000

    UrlComponentListLocator now supports HTTP parameters.

--- components/src/org/riotfamily/components/locator/UrlComponentListLocator.java
+++ components/src/org/riotfamily/components/locator/UrlComponentListLocator.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2007
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.components.locator;
 
@@ -29,13 +29,13 @@ import org.riotfamily.common.web.util.PathCompleter;
 import org.riotfamily.common.web.util.ServletUtils;
 
 /**
- * ComponentListLocator that calls 
- * {@link ServletUtils#getOriginatingPathWithoutServletMapping(HttpServletRequest)} 
+ * ComponentListLocator that calls
+ * {@link ServletUtils#getOriginatingPathWithoutServletMapping(HttpServletRequest)}
  * to determine the lookup-path for a given request.
  * <p>
- * Example: If the DispatcherServlet is mapped to <code>*.html</code> and 
+ * Example: If the DispatcherServlet is mapped to <code>*.html</code> and
  * <code>/myapp</code> is the application's context path, the component-path
- * for a request to <code>/myapp/foo/bar.html</code> will be 
+ * for a request to <code>/myapp/foo/bar.html</code> will be
  * <code>/foo/bar</code>.
  * </p>
  * @since 6.5
@@ -43,21 +43,36 @@ import org.riotfamily.common.web.util.ServletUtils;
 public class UrlComponentListLocator extends AbstractComponentListLocator {
 
 	public static final String TYPE_URL = "url";
-	
+
 	private PathCompleter pathCompleter;
-	
+
+	private String parameter;
+
 	public UrlComponentListLocator(PathCompleter pathCompleter) {
 		super(TYPE_URL);
 		this.pathCompleter = pathCompleter;
 	}
 
-	protected String getPath(HttpServletRequest request) {
-		return ServletUtils.getOriginatingPathWithoutServletMapping(request);
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
 	}
-	
+
+	protected String getPath(HttpServletRequest request) {
+		String path = ServletUtils.getOriginatingPathWithoutServletMapping(request);
+		if (parameter != null) {
+			String value = request.getParameter(parameter);
+			if (value != null) {
+				StringBuffer sb = new StringBuffer(path);
+				sb.append('?').append(parameter).append('=').append(value);
+				return sb.toString();
+			}
+		}
+		return path;
+	}
+
 	/**
 	 * Returns a substring of the given path starting at zero and ending before
-	 * the last slash character. If no slash is found or the only slash is 
+	 * the last slash character. If no slash is found or the only slash is
 	 * at the beginning of the path, <code>null</code> is returned.
 	 */
 	protected String getParentPath(String path) {
@@ -67,7 +82,7 @@ public class UrlComponentListLocator extends AbstractComponentListLocator {
 		}
 		return null;
 	}
-	
+
 	protected String getUrlForPath(String path) {
 		return pathCompleter.addServletMapping(path);
 	}
