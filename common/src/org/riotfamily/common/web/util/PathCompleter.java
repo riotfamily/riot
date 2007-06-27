77c6a49f036c6d60b50fe2d0933b0050e98b28a2
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-27 11:52:00 +0000

    The PathCompleter now supports URLs with a query string.

--- common/src/org/riotfamily/common/web/util/PathCompleter.java
+++ common/src/org/riotfamily/common/web/util/PathCompleter.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2007
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.web.util;
 
@@ -35,24 +35,24 @@ import org.springframework.web.context.ServletContextAware;
 /**
  * Service class that can be used to add the servlet-prefix or suffix to a
  * path without having a request object.
- * 
+ *
  * @author Felix Gnass [fgnass at neteye dot de]
  * @since 6.4
  */
 public class PathCompleter implements ServletContextAware, InitializingBean {
 
 	private static final Log log = LogFactory.getLog(PathCompleter.class);
-	
+
 	private String servletName;
-	
+
 	private String servletMapping;
-	
+
 	private ServletContext servletContext;
-	
+
 	private String servletPrefix;
-	
+
 	private String servletSuffix;
-		
+
 	public void setServletContext(ServletContext servletContext) {
 		this.servletContext = servletContext;
 	}
@@ -69,14 +69,14 @@ public class PathCompleter implements ServletContextAware, InitializingBean {
 		if (servletMapping == null) {
 			Assert.notNull(servletName, "Either servletMapping or "
 					+ "servletName must be set.");
-			
-			servletMapping = ServletUtils.getServletMapping(servletName, 
+
+			servletMapping = ServletUtils.getServletMapping(servletName,
 					servletContext);
-			
-			Assert.notNull(servletMapping, "Could not determine mapping " 
+
+			Assert.notNull(servletMapping, "Could not determine mapping "
 					+ "for servlet '" + servletName + "'.");
-		
-			log.info("Servlet '" + servletName + "' is mapped to " 
+
+			log.info("Servlet '" + servletName + "' is mapped to "
 					+ servletMapping + " in web.xml");
 		}
 		servletSuffix = "";
@@ -91,14 +91,26 @@ public class PathCompleter implements ServletContextAware, InitializingBean {
 		log.info("Servlet prefix: '" + servletPrefix + "'");
 		log.info("Servlet suffix: '" + servletSuffix + "'");
 	}
-	
+
 	public String addServletMapping(String path) {
+		int i = path.indexOf('?');
+		if (i != -1 && servletSuffix.length() > 0) {
+			return path.substring(0, i) + servletSuffix + path.substring(i);
+		}
 		return servletPrefix + path + servletSuffix;
 	}
-	
+
 	public void addServletMapping(StringBuffer path) {
 		path.insert(0, servletPrefix);
-		path.append(servletSuffix);
+		if (servletSuffix.length() > 0) {
+			int i = path.indexOf("?");
+			if (i != -1) {
+				path.insert(i, servletSuffix);
+			}
+			else {
+				path.append(servletSuffix);
+			}
+		}
 	}
-		
+
 }
