1c32f038fc601f037b4d933df175277c9965b3af
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-29 16:15:25 +0000

    Using the TransactionalController interface instead of the PlatformTransactionManager.

--- riot/src/org/riotfamily/riot/list/export/CsvExportController.java
+++ riot/src/org/riotfamily/riot/list/export/CsvExportController.java
@@ -4,22 +4,22 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2007
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Jan-Frederic Linde [jfl at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.list.export;
 
@@ -32,6 +32,7 @@ import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.riotfamily.common.web.transaction.TransactionalController;
 import org.riotfamily.riot.dao.ListParams;
 import org.riotfamily.riot.editor.EditorDefinitionUtils;
 import org.riotfamily.riot.editor.EditorRepository;
@@ -40,106 +41,72 @@ import org.riotfamily.riot.list.ListConfig;
 import org.riotfamily.riot.list.support.ListParamsImpl;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class CsvExportController implements Controller {
-	
+public class CsvExportController implements TransactionalController {
+
 	private EditorRepository editorRepository;
-	
-	private PlatformTransactionManager transactionManager;
-	
+
 	private String encoding = "UTF-8";
-	
+
 	private String delimiter = ";";
-	
+
 	public CsvExportController(EditorRepository editorRepository) {
 		this.editorRepository = editorRepository;
 	}
-	
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
-	
+
 	public void setDelimiter(String delimiter) {
 		this.delimiter = delimiter;
 	}
-	
+
 	public void setEncoding(String encoding) {
 		this.encoding = encoding;
 	}
-	
-	public ModelAndView handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-		if (transactionManager != null) {
-			new TransactionTemplate(transactionManager).execute(new TransactionCallback() {
-				public Object doInTransaction(TransactionStatus status) {
-					try {
-						generateCsv(request, response);
-					}
-					catch (Exception e) {
-					}
-					return null;
-				}
-			});
-		}
-		else {
-			generateCsv(request, response);
-		}
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		generateCsv(request, response);
 		return null;
 	}
 
-	
 	public void generateCsv(HttpServletRequest request,
-					HttpServletResponse response) throws Exception {
-		
-		String listId = ServletRequestUtils.getStringParameter(request, 
-							"listId");
-		
-		String commandId = ServletRequestUtils.getStringParameter(request, 
-							"commandId");
-		
-		String parentId = ServletRequestUtils.getStringParameter(request, 
-							"parentId");
-		
-		CsvExportCommand command = (CsvExportCommand) editorRepository
-				.getListRepository().getCommand(commandId);
-		
+			HttpServletResponse response) throws Exception {
+
+		String listId = ServletRequestUtils.getStringParameter(request, "listId");
+		String commandId = ServletRequestUtils.getStringParameter(request, "commandId");
+		String parentId = ServletRequestUtils.getStringParameter(request, "parentId");
+
+		CsvExportCommand command = (CsvExportCommand)
+				editorRepository.getListRepository().getCommand(commandId);
+
 		String fileEncoding = encoding;
 		if (command.getEncoding() != null) {
 			fileEncoding = command.getEncoding();
 		}
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
-		
+
 		String fileName = listId + '-' + sdf.format(new Date()) + ".csv";
-		
+
 		response.setContentType("text/csv; charset=" + fileEncoding);
 		response.setCharacterEncoding(encoding);
-		response.setHeader("Content-Disposition",
-						"attachment; filename=" + fileName);
-		
+		response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+
 		PrintWriter out = response.getWriter();
-		
+
 		for (int i = 0; i < command.getProperties().size(); i++) {
 			out.print(command.getProperties().get(i));
 			out.print(delimiter);
 		}
 		out.println();
-		
-		ListDefinition listDefinition = editorRepository
-					.getListDefinition(listId);
-		
-		ListConfig listConfig = editorRepository.getListRepository()
-				.getListConfig(listId);
-		
-		Object parent = EditorDefinitionUtils.loadParent(
-					listDefinition, parentId);
-		
+
+		ListDefinition listDefinition = editorRepository.getListDefinition(listId);
+		ListConfig listConfig = listDefinition.getListConfig();
+
+		Object parent = EditorDefinitionUtils.loadParent(listDefinition, parentId);
+
 		ListParams params = new ListParamsImpl();
 		Collection beans = listConfig.getDao().list(parent, params);
 		Iterator beanIterator = beans.iterator();
@@ -149,7 +116,7 @@ public class CsvExportController implements Controller {
 				String propertyName = (String) command.getProperties().get(i);
 				Object value = bean.getPropertyValue(propertyName);
 				if (value instanceof Collection) {
-					Iterator it = ((Collection)value).iterator();
+					Iterator it = ((Collection) value).iterator();
 					while (it.hasNext()) {
 						out.print(it.next());
 						out.print(' ');
@@ -158,7 +125,7 @@ public class CsvExportController implements Controller {
 				else if (value != null) {
 					out.print(value);
 				}
-				
+
 				out.print(delimiter);
 			}
 			out.println();
