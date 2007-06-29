93c8940ad1ff13dc91f7c79ed0b813a294d8758f
Felix Gnass <felix.gnass@riotfamily.org>
2007-06-29 10:26:44 +0000

    Added a utility method to read a Resource as String.

--- common/src/org/riotfamily/common/util/ResourceUtils.java
+++ common/src/org/riotfamily/common/util/ResourceUtils.java
@@ -4,37 +4,45 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
  * License.
- * 
+ *
  * The Original Code is Riot.
- * 
+ *
  * The Initial Developer of the Original Code is
  * Neteye GmbH.
  * Portions created by the Initial Developer are Copyright (C) 2006
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s):
  *   Felix Gnass [fgnass at neteye dot de]
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.util;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+
 /**
  * Provides utility method to work with classpath resources.
- *   
+ *
  * @author Felix Gnass [fgnass at neteye dot de]
  */
 public final class ResourceUtils {
 
 	private static final String CLASSPATH_PREFIX = "classpath:";
-	
+
 	private ResourceUtils() {
 	}
-	
+
 	public static String getPath(Class clazz, String name) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(CLASSPATH_PREFIX).append('/');
@@ -45,6 +53,13 @@ public final class ResourceUtils {
 		sb.append('/');
 		sb.append(name);
 		return sb.toString();
+	}
+
+	public static String readResource(Resource resource, String encoding)
+			throws UnsupportedEncodingException, IOException {
+
+		Reader in = new InputStreamReader(resource.getInputStream(), encoding);
+		return FileCopyUtils.copyToString(in);
 	}
 
 }
