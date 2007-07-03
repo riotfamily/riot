d12ad5c36d9f0083049840d3e3a3188f05192b07
Carsten Wölk <carsten.woelk@riotfamily.org>
2007-07-03 18:18:55 +0000

    Aligned the group to test for page.isEnabled instead of just published

--- pages/src/org/riotfamily/pages/macro/PageMacroHelper.java
+++ pages/src/org/riotfamily/pages/macro/PageMacroHelper.java
@@ -1,26 +1,26 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Riot.
- *
- * The Initial Developer of the Original Code is
- * Neteye GmbH.
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Felix Gnass [fgnass at neteye dot de]
- *
- * ***** END LICENSE BLOCK ***** */
+	* Version: MPL 1.1
+	* The contents of this file are subject to the Mozilla Public License Version
+	* 1.1 (the "License"); you may not use this file except in compliance with
+	* the License. You may obtain a copy of the License at
+	* http://www.mozilla.org/MPL/
+	*
+	* Software distributed under the License is distributed on an "AS IS" basis,
+	* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	* for the specific language governing rights and limitations under the
+	* License.
+	*
+	* The Original Code is Riot.
+	*
+	* The Initial Developer of the Original Code is
+	* Neteye GmbH.
+	* Portions created by the Initial Developer are Copyright (C) 2007
+	* the Initial Developer. All Rights Reserved.
+	*
+	* Contributor(s):
+	*   Felix Gnass [fgnass at neteye dot de]
+	*
+	* ***** END LICENSE BLOCK ***** */
 package org.riotfamily.pages.macro;
 
 import java.util.ArrayList;
@@ -43,9 +43,9 @@ import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 /**
- * @author Felix Gnass [fgnass at neteye dot de]
- * @since 6.5
- */
+	* @author Felix Gnass [fgnass at neteye dot de]
+	* @since 6.5
+	*/
 public class PageMacroHelper {
 
 	private static final String PAGE_REFRESHED = PageMacroHelper.class.getName()
@@ -123,7 +123,7 @@ public class PageMacroHelper {
 		Iterator it = pages.iterator();
 		while (it.hasNext()) {
 			Page page = (Page) it.next();
-			if (!page.getNode().isHidden() && (page.isPublished()
+			if (!page.getNode().isHidden() && (page.isEnabled()
 					|| EditModeUtils.isEditMode(request))) {
 
 				if (i++ % size == 0) {
