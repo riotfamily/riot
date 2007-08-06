68afcab6c6f21d4def176a96ced3deb29d352add
Felix Gnass <felix.gnass@riotfamily.org>
2007-08-06 16:27:06 +0000

    Anti-Aliasing can now be turned off (useful if rendering pixel fonts). Additionally fractional metrics are now enabled.

--- website/src/org/riotfamily/website/txt2img/ImageGenerator.java
+++ website/src/org/riotfamily/website/txt2img/ImageGenerator.java
@@ -71,6 +71,8 @@ public class ImageGenerator implements InitializingBean {
 	
 	private Color color = Color.BLACK;
 	
+	private boolean antiAlias = true;
+	
 	private FlatMap attributes = new FlatMap();
 	
 	/**
@@ -109,6 +111,13 @@ public class ImageGenerator implements InitializingBean {
 		this.size = size;
 	}
 
+	/**
+	 * Turns anti-aliasing off or on (default).
+	 */
+	public void setAntiAlias(boolean antiAlias) {
+		this.antiAlias = antiAlias;
+	}
+	
 	/**
 	 * Sets the interline spacing in pixels. If not set, 
 	 * {@link TextLayout#getLeading()} is used.
@@ -224,8 +233,31 @@ public class ImageGenerator implements InitializingBean {
 	
 	protected Graphics2D createGraphics(BufferedImage image) {
         Graphics2D graphics = image.createGraphics();
-        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
-        		RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, antiAlias 
+        		? RenderingHints.VALUE_ANTIALIAS_ON
+        		: RenderingHints.VALUE_ANTIALIAS_OFF);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, antiAlias 
+        		? RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+        		: RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, antiAlias 
+        		? RenderingHints.VALUE_FRACTIONALMETRICS_ON
+        		: RenderingHints.VALUE_FRACTIONALMETRICS_OFF);
+        
+        // Quality settings
+        
+        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+           		RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, 
+        		RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, 
+        		RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_RENDERING, 
+        		RenderingHints.VALUE_RENDER_QUALITY);
         
         return graphics;
 	}
