f73b09f34978ab6cb71ff8f8f4651c4af46ca5fa
Felix Gnass <felix.gnass@riotfamily.org>
2007-08-16 16:09:29 +0000

    The IncludeOnlyInterceptor now extends PathMatchingInterceptor.

--- website/src/org/riotfamily/website/interceptor/IncludeOnlyInterceptor.java
+++ website/src/org/riotfamily/website/interceptor/IncludeOnlyInterceptor.java
@@ -27,25 +27,27 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.riotfamily.common.web.util.PathMatchingInterceptor;
 import org.springframework.web.util.WebUtils;
 
 /**
  * HandlerInterceptor that sends a 404 error if the request is not an 
- * include-request, i.e. was not dispatched by a {@link RequestDispatcher}.
+ * include request, i.e. was not dispatched by a {@link RequestDispatcher}.
  * 
  * @author Felix Gnass [fgnass at neteye dot de]
  */
-public class IncludeOnlyInterceptor extends HandlerInterceptorAdapter {
+public class IncludeOnlyInterceptor extends PathMatchingInterceptor {
 
-	public boolean preHandle(HttpServletRequest request, 
+	private static Log log = LogFactory.getLog(IncludeOnlyInterceptor.class);
+	
+	protected boolean doPreHandle(HttpServletRequest request, 
 			HttpServletResponse response, Object handler) throws Exception {
 		
 		if (!WebUtils.isIncludeRequest(request)) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND, 
-					"This URL can be requested through a " +
-					"RequestDispatcher only.");
-			
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			log.warn("Direct access prevented by IncludeOnlyInterceptor");
 			return false;
 		}
 		return true;
