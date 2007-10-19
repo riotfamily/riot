15838c2a83bd69a38a1a90a525626fde4fe78d02
Felix Gnass <felix.gnass@riotfamily.org>
2007-10-19 09:31:12 +0000

    The getWriter() method now always returns the same instance.

--- website/src/org/riotfamily/website/interceptor/pushup/DeferredRenderingResponseWrapper.java
+++ website/src/org/riotfamily/website/interceptor/pushup/DeferredRenderingResponseWrapper.java
@@ -29,6 +29,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
@@ -48,7 +49,11 @@ public class DeferredRenderingResponseWrapper extends HttpServletResponseWrapper
 	
 	private StringWriter writer;
 	
-	private boolean redirectSent = false;
+	private PrintWriter printWriter;
+	
+	private boolean redirectSent;
+	
+	private boolean flush;
 	
 	public DeferredRenderingResponseWrapper(HttpServletResponse response) {
 		super(response);		
@@ -73,8 +78,9 @@ public class DeferredRenderingResponseWrapper extends HttpServletResponseWrapper
 		if (outputStream == null) {
 			if (writer == null) {
 				writer = new StringWriter();
+				printWriter = new PrintWriter(writer);
 			}
-			return new PrintWriter(writer);
+			return printWriter;
 		}
 		else {
 			throw new IllegalStateException(
@@ -95,17 +101,28 @@ public class DeferredRenderingResponseWrapper extends HttpServletResponseWrapper
 		}
 	}
 	
+	public void flushBuffer() throws IOException {
+		flush = true;
+	}
+	
 	public boolean isRedirectSent() {		
 		return redirectSent;
 	}
 	
-	public void renderResponse(HttpServletResponse response) throws IOException {
+	public void renderResponse() throws IOException {
+		renderResponse(getResponse());
+	}
+	
+	public void renderResponse(ServletResponse response) throws IOException {
 		if (outputStream != null) {			
 			FileCopyUtils.copy(outputStream.toByteArray(),
 					response.getOutputStream());
 		}
 		else if (writer != null) {
 			response.getWriter().write(writer.toString());
+		}
+		if (flush) {
+			super.flushBuffer();
 		}
 	}
 	
