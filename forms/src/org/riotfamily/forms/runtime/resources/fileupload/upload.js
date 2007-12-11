fd7ef179a51330f3b6b1293948f54b26ca2f04c4
Felix Gnass <felix.gnass@riotfamily.org>
2007-12-11 10:37:58 +0000

    Fixed the inline file upload in IE6.

--- forms/src/org/riotfamily/forms/runtime/resources/fileupload/upload.js
+++ forms/src/org/riotfamily/forms/runtime/resources/fileupload/upload.js
@@ -1,37 +1,30 @@
 function uploadInline(e, uploadUrl) {
-	if (!submitEvent) {
+	if (!window.submitEvent) {
 		f.submit();
 		return;
 	}
 	var f = e.form;
+    var originalAction = f.action;
+    f.action = uploadUrl;
+    
+	if (!window.frames[e.id]) {
+		var divIframe = document.createElement('div');
+		divIframe.style.width = '1px';
+		divIframe.style.height = '1px';
+		divIframe.style.visibility = 'hidden';
+		divIframe.style.position = 'absolute';
+		divIframe.innerHTML = '<iframe name="' + e.id + '" width="1" height="1"></iframe>';		
+		document.body.appendChild(divIframe);				
+	}
 	
-	if (typeof XMLHttpRequest == 'undefined') {
-		f.submit();
+	var originalTarget = f.target;
+	if (window.frames[e.id]) {		
+		f.target = e.id;
+		f.action += '&_exclusive=' + e.id;		
 	}
-	else {
-	    var originalAction = f.action;
-	    f.action = uploadUrl;
-	    
-		if (!window.frames[e.id]) {
-			var divIframe = document.createElement('div');
-			divIframe.style.width = '1px';
-			divIframe.style.height = '1px';
-			divIframe.style.visibility = 'hidden';
-			divIframe.style.position = 'absolute';
-			divIframe.innerHTML = '<iframe name="' + e.id + '" width="1" height="1"></iframe>';		
-			document.body.appendChild(divIframe);				
-		}
-		
-		var originalTarget = f.target;
-		if (window.frames[e.id]) {		
-			f.target = e.id;
-			f.action += '&_exclusive=' + e.id;		
-		}
-		
-		f.submit();
-		
-		f.target = originalTarget;
-		f.action = originalAction;
-		setTimeout(function() { submitEvent(new ChangeEvent(e)); }, 1000);
-	}
+	f.submit();
+	
+	f.target = originalTarget;
+	f.action = originalAction;
+	setTimeout(function() { submitEvent(new ChangeEvent(e)); }, 1000);
 }
