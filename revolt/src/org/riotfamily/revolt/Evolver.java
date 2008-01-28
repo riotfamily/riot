a5b081c360622fe21487ee306f7cfc75c2156812
Felix Gnass <felix.gnass@riotfamily.org>
2008-01-28 12:29:37 +0000

    Organized the imports.

--- revolt/src/org/riotfamily/revolt/Evolver.java
+++ revolt/src/org/riotfamily/revolt/Evolver.java
@@ -23,13 +23,9 @@
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.revolt;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
