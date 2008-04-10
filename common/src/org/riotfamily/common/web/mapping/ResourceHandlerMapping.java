39712587d62323a1b0b0ae8ec14815eaf9f7ef52
Felix Gnass <felix.gnass@riotfamily.org>
2008-04-10 18:28:29 +0000

    Added a HandlerMapping implementation to serve static resources.

---
+++ common/src/org/riotfamily/common/web/mapping/ResourceHandlerMapping.java
@@ -0,0 +1,103 @@
+package org.riotfamily.common.web.mapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.activation.FileTypeMap;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.riotfamily.common.io.IOUtils;
+import org.riotfamily.common.web.util.ServletUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.support.ServletContextResource;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.AbstractHandlerMapping;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.LastModified;
+
+/**
+ * HandlerMapping that serves static resources from the application directory.
+ * <p>
+ * This can be useful if you map your DispatcherServlet to *.xml in order to
+ * serve dynamic XML files but still have some static resources that would 
+ * otherwise be handled by the default servlet.
+ * </p>
+ * <p>
+ * For security reasons no resources under WEB-INF or outside of the 
+ * application directory will be served.
+ * </p> 
+ * @author Felix Gnass [fgnass at neteye dot de]
+ */
+public class ResourceHandlerMapping extends AbstractHandlerMapping {
+
+	public static final Pattern FORBIDDEN = Pattern.compile(
+			"(WEB-INF|META-INF|\\.\\.)", Pattern.CASE_INSENSITIVE);
+	
+	private FileTypeMap fileTypeMap;
+	
+	public void setFileTypeMap(FileTypeMap fileTypeMap) {
+		this.fileTypeMap = fileTypeMap;
+	}
+
+	protected Object getHandlerInternal(HttpServletRequest request)
+			throws Exception {
+		
+		if (ServletUtils.isDirectRequest(request)) {
+			String path = ServletUtils.getOriginatingPathWithinApplication(request);
+			path = StringUtils.cleanPath(path);
+			Resource res = new ServletContextResource(getServletContext(), path);
+			if (res.exists()) {
+				if (!FORBIDDEN.matcher(path).find()) {
+					String contentType = null;
+					if (fileTypeMap != null) {
+						contentType = fileTypeMap.getContentType(path);
+					}
+					return new ServeResourceCotroller(res, contentType);
+				}
+			}
+		}
+		return null;
+	}
+	
+	private static class ServeResourceCotroller 
+			implements Controller, LastModified {
+		
+		private static final long STARTUP_TIME = System.currentTimeMillis();
+		
+		private Resource res;
+		
+		private String contentType;
+		
+		private long lastModified;
+		
+		public ServeResourceCotroller(Resource res, String contentType) {
+			this.res = res;
+			this.contentType = contentType;
+			try {
+				File f = res.getFile();
+				lastModified = f.lastModified();
+			}
+			catch (IOException ex) {
+				lastModified = STARTUP_TIME;
+			}
+		}
+
+		public long getLastModified(HttpServletRequest request) {
+			return lastModified;
+		}
+		
+		public ModelAndView handleRequest(HttpServletRequest request,
+				HttpServletResponse response) throws Exception {
+			
+			if (contentType != null) {
+				response.setContentType(contentType);
+			}
+			IOUtils.serve(res.getInputStream(), response.getOutputStream());
+			return null;
+		}
+		
+	}
+}
