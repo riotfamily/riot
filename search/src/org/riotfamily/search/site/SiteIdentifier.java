8051df9625fa95d94f4185fdaeaa017401bf586f
Alf Werder <alf.werder@artundweise.de>
2008-06-13 15:37:04 +0000

    Eagerly initialize sites aliases for later use.

--- search/src/org/riotfamily/search/site/SiteIdentifier.java
+++ search/src/org/riotfamily/search/site/SiteIdentifier.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.Hibernate;
 import org.riotfamily.common.web.util.ServletUtils;
 import org.riotfamily.pages.dao.PageDao;
 import org.riotfamily.pages.model.Site;
