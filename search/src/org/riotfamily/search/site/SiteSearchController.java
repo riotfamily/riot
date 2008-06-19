6b3942c390be8b57eeb97cddd045391f28842d07
Alf Werder <alf.werder@artundweise.de>
2008-06-19 13:04:52 +0000

    SiteSearchController merges site id filter into generic filters.

--- search/src/org/riotfamily/search/site/SiteSearchController.java
+++ search/src/org/riotfamily/search/site/SiteSearchController.java
@@ -3,8 +3,10 @@ package org.riotfamily.search.site;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.BooleanClause.Occur;
 import org.riotfamily.pages.mapping.PageResolver;
 import org.riotfamily.pages.model.Page;
 import org.riotfamily.search.SearchController;
@@ -17,11 +19,26 @@ import org.riotfamily.search.SearchController;
 public class SiteSearchController extends SearchController {
 
 	protected Query createFilterQuery(HttpServletRequest request) {
+	    Query query = null;
+	    
 		Page currentPage = PageResolver.getResolvedPage(request);
 		if (currentPage != null) {
 			String siteId = currentPage.getSite().getId().toString();
-			return new TermQuery(new Term("siteId", siteId));
+			
+			query = new TermQuery(new Term("siteId", siteId));
 		}
-		return null;
+		
+		return combineFilterQueries(query, super.createFilterQuery(request));
+	}
+	
+	private Query combineFilterQueries(Query q, Query r) {
+	    if (q == null && r == null) return null;
+	    if (q == null) return r;
+	    if (r == null) return q;
+
+	    BooleanQuery query = new BooleanQuery();
+        query.add(q, Occur.MUST);
+        query.add(r, Occur.MUST);
+        return query;
 	}
 }
