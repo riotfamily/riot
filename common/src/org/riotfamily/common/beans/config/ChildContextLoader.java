2b218e7fa3537e6d6b38d975b0f12ea48c75667f
Felix Gnass <felix.gnass@riotfamily.org>
2008-08-07 15:03:44 +0000

    Moved to the config package

--- common/src/org/riotfamily/common/beans/config/ChildContextLoader.java
+++ common/src/org/riotfamily/common/beans/config/ChildContextLoader.java
@@ -21,7 +21,7 @@
  *   Felix Gnass [fgnass at neteye dot de]
  * 
  * ***** END LICENSE BLOCK ***** */
-package org.riotfamily.common.beans;
+package org.riotfamily.common.beans.config;
 
 import javax.servlet.ServletContext;
 
