796a0b1885d35a8586222a589307f7c71f834f4e
Felix Gnass <felix.gnass@riotfamily.org>
2008-08-14 14:14:08 +0000

    The crawler now uses the new scheduling infrastructure

---
+++ common/src/org/riotfamily/common/scheduling/MethodInvokingScheduledTask.java
@@ -0,0 +1,55 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Riot.
+ *
+ * The Initial Developer of the Original Code is
+ * Neteye GmbH.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Felix Gnass [fgnass at neteye dot de]
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.riotfamily.common.scheduling;
+
+import org.springframework.core.Ordered;
+import org.springframework.util.MethodInvoker;
+
+public class MethodInvokingScheduledTask extends MethodInvoker 
+		implements ScheduledTask, Ordered {
+
+	private String[] triggerNames;
+	
+	private int order = Ordered.LOWEST_PRECEDENCE;
+	
+	public String[] getTriggerNames() {
+		return triggerNames;
+	}
+	
+	public void setTriggerNames(String[] triggerNames) {
+		this.triggerNames = triggerNames;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	
+	public void execute() throws Exception {
+		invoke();
+	}
+}
