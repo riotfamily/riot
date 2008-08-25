c878f6c235dc7510afe11bfa8cd36c403677c923
Jan-Frederic Linde <jan-frederic.linde@riotfamily.org>
2008-08-25 22:16:18 +0000

    Added import and export for message bundle entries

---
+++ dbmsgsrc/src/org/riotfamily/dbmsgsrc/riot/ImportMessageEntriesCommand.java
@@ -0,0 +1,135 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Riot.
+ *
+ * The Initial Developer of the Original Code is
+ * Neteye GmbH.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Jan-Frederic Linde [jfl at neteye dot de]
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.riotfamily.dbmsgsrc.riot;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.riotfamily.dbmsgsrc.dao.DbMessageSourceDao;
+import org.riotfamily.dbmsgsrc.model.MessageBundleEntry;
+import org.riotfamily.dbmsgsrc.support.DbMessageSource;
+import org.riotfamily.forms.Form;
+import org.riotfamily.forms.element.upload.FileUpload;
+import org.riotfamily.riot.list.command.CommandContext;
+import org.riotfamily.riot.list.command.dialog.DialogCommand;
+import org.riotfamily.riot.list.ui.ListSession;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ImportMessageEntriesCommand extends DialogCommand {
+
+public static final String ACTION_IMPORT = "import";
+
+	private static final Log log = LogFactory.getLog(ImportMessageEntriesCommand.class);
+
+	private DbMessageSourceDao dao;
+	
+	private String bundle = DbMessageSource.DEFAULT_BUNDLE;
+	
+	public ImportMessageEntriesCommand(DbMessageSourceDao dao) {
+		this.dao = dao;
+	}
+
+	public void setBundle(String bundle) {
+		this.bundle = bundle;
+	}
+	
+	@Override
+	protected String getAction(CommandContext context) {
+		return ACTION_IMPORT;
+	}
+	
+	@Override
+	public Form createForm(Object bean) {
+		Form form = new Form(Upload.class);
+		FileUpload fileUpload = new FileUpload();
+		fileUpload.setRequired(true);
+		form.addElement(fileUpload, "data");
+		return form;
+	}
+	
+	@Override
+	public ModelAndView handleInput(Object input, ListSession listSession) {		
+		Upload upload = (Upload) input;
+		try {
+			updateMessages(upload.getData());
+		} catch (IOException e) {			
+		}
+		return null;
+	}
+	
+	private void updateMessages(byte[] data) throws IOException {
+		HSSFWorkbook wb = new HSSFWorkbook(new ByteArrayInputStream(data));
+		HSSFSheet sheet = wb.getSheet("Messages");
+		
+		if (isValid(sheet)) {
+			for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+				HSSFRow row = sheet.getRow(i);
+				String code = row.getCell(1).getRichStringCellValue().getString();
+				String defaultMessage = row.getCell(2).getRichStringCellValue().getString();
+				String comment = row.getCell(3).getRichStringCellValue().getString();
+				if (StringUtils.hasText(defaultMessage) || StringUtils.hasText(comment)) {
+					MessageBundleEntry entry = dao.findEntry(bundle, code);
+					if (entry != null) {
+						entry.getDefaultMessage().setText(defaultMessage);
+						entry.setComment(comment);
+						dao.saveEntry(entry);					
+					}
+					else {
+						log.info("Message Code does not exist - " + code);
+					}
+				}				
+			}
+		}
+	}
+	
+	private boolean isValid(HSSFSheet sheet) {
+		HSSFRow headings = sheet.getRow(0);
+		String code = headings.getCell(1).getRichStringCellValue().getString();
+		String defaultMessage = headings.getCell(2).getRichStringCellValue().getString();
+		String comment = headings.getCell(3).getRichStringCellValue().getString();
+		return "Code".equals(code) && "Default Message".equals(defaultMessage)
+				&& "Comment".equals(comment);
+		
+	}
+	
+	public static class Upload {
+		
+		private byte[] data;
+
+		public byte[] getData() {
+			return data;
+		}
+
+		public void setData(byte[] data) {
+			this.data = data;
+		}
+		
+	}
+}
---
+++ dbmsgsrc/src/org/riotfamily/dbmsgsrc/riot/MessageEntryExcelExporter.java
@@ -0,0 +1,147 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Riot.
+ *
+ * The Initial Developer of the Original Code is
+ * Neteye GmbH.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Jan-Frederic Linde [jfl at neteye dot de]
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.riotfamily.dbmsgsrc.riot;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.riotfamily.dbmsgsrc.model.MessageBundleEntry;
+import org.riotfamily.riot.list.command.export.Exporter;
+
+public class MessageEntryExcelExporter implements Exporter {
+
+	
+	public String getFileExtension() {
+		return "xls";
+	}
+	
+	public void export(Collection<?> items, Object parent, 
+			List<String> properties, HttpServletResponse response) 
+			throws IOException {
+				
+		HSSFWorkbook wb = new WorkbookCreator().createWorkbook(items);
+		wb.write(response.getOutputStream());
+	}
+	
+	private final static class WorkbookCreator {
+		
+		HSSFWorkbook wb;
+		
+		HSSFSheet sheet; 
+		
+		HSSFCellStyle locked;
+		
+		HSSFCellStyle editable;
+		
+		HSSFCellStyle hidden;
+		
+		private HSSFWorkbook createWorkbook(Collection<?> items) {
+			wb = new HSSFWorkbook();
+
+			sheet = wb.createSheet("Messages");
+			//sheet.protectSheet("");
+
+			locked = wb.createCellStyle();
+			locked.setLocked(true);
+			locked.setWrapText(true);
+			locked.setVerticalAlignment(HSSFCellStyle.VERTICAL_TOP);
+
+			editable = wb.createCellStyle();
+			editable.setLocked(false);
+			editable.setWrapText(true);
+			editable.setVerticalAlignment(HSSFCellStyle.VERTICAL_TOP);
+			
+			hidden = wb.createCellStyle();
+			hidden.setLocked(true);
+			
+			createHeadings("Category", "Code", "Default Message", "Comment");
+			createRows(items);
+			return wb;
+		}
+		
+		private void createHeadings(String... labels) {
+			HSSFCellStyle style = wb.createCellStyle();
+		    HSSFFont font = wb.createFont();
+		    font.setFontName("Trebuchet MS");
+		    font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+		    style.setFont(font);
+		    
+			HSSFRow row = sheet.createRow(0);
+			short col = 0;
+			for (String label : labels) {
+				 HSSFCell cell = row.createCell(col++);
+				 cell.setCellStyle(style);
+				 cell.setCellValue(new HSSFRichTextString(label));
+			}
+		}
+
+		private void createRows(Collection<?> items) {
+			int i = 1;
+			for (Object item : items) {
+				MessageBundleEntry entry = (MessageBundleEntry) item;				
+				HSSFRow row = sheet.createRow(i++);
+				addCell(row, 0, getCategory(entry), editable);
+				addCell(row, 1, entry.getCode(), locked);
+				addCell(row, 2, entry.getDefaultText(), locked);				
+				addCell(row, 3, entry.getComment(), editable);				
+			}
+
+			sheet.autoSizeColumn((short) 0);
+			sheet.setColumnWidth((short) 1, (short) (25 * 256));
+			sheet.setColumnWidth((short) 2, (short) (50 * 256));
+			sheet.setColumnWidth((short) 3, (short) (50 * 256));			
+		}
+
+		private String getCategory(MessageBundleEntry entry) {
+			String category = entry.getCode();
+			int i = category.indexOf('.');
+			if (i != -1) {
+				category = category.substring(0, i);
+			}
+			return category;
+		}
+
+		private void addCell(HSSFRow row, int i, String text, HSSFCellStyle style) {
+			HSSFCell cell = row.createCell((short) i);
+			cell.setCellStyle(style);
+			cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+			if (text == null) {
+				text = "";
+			}
+			cell.setCellValue(new HSSFRichTextString(text));
+		}
+		
+	}
+
+}
