053c6ab4c4cb7abb60521220a483c95ff30bb456
Felix Gnass <felix.gnass@riotfamily.org>
2008-08-27 19:43:45 +0000

    Removed the hqlOptionsModel prototype which is now longer needed as models an now be auto-wired.

--- riot-hibernate/src/org/riotfamily/riot/hibernate/form/HqlOptionsModel.java
+++ riot-hibernate/src/org/riotfamily/riot/hibernate/form/HqlOptionsModel.java
@@ -25,6 +25,7 @@ package org.riotfamily.riot.hibernate.form;
 
 import java.util.Collection;
 
+import org.hibernate.SessionFactory;
 import org.riotfamily.forms.Element;
 import org.riotfamily.forms.options.OptionsModel;
 import org.riotfamily.riot.hibernate.support.HibernateSupport;
@@ -32,6 +33,10 @@ import org.riotfamily.riot.hibernate.support.HibernateSupport;
 public class HqlOptionsModel extends HibernateSupport implements OptionsModel {
 
 	private String hql;
+	
+	public HqlOptionsModel(SessionFactory sessionFactory) {
+		setSessionFactory(sessionFactory);
+	}
 	
 	public void setHql(String hql) {
 		this.hql = hql;
