cf5a4a350ce95286a03ed5388afe5cfe9b617629
Felix Gnass <felix.gnass@riotfamily.org>
2008-09-11 09:39:30 +0000

    The EntityWrapper now uses a custom Hibernate type to avoid ObjectNotFoundExceptions

--- components/src/org/riotfamily/components/model/wrapper/EntityWrapper.java
+++ components/src/org/riotfamily/components/model/wrapper/EntityWrapper.java
@@ -3,13 +3,11 @@ package org.riotfamily.components.model.wrapper;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
 
-import org.hibernate.annotations.Any;
-import org.hibernate.annotations.AnyMetaDef;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.Type;
 
 @Entity
 @DiscriminatorValue("Entity")
@@ -24,9 +22,24 @@ public class EntityWrapper extends ValueWrapper<Object> {
 		return copy;
 	}
 
-	@Any(metaColumn=@Column(name="entity_table"), fetch=FetchType.LAZY)
-	@AnyMetaDef(idType="long", metaType="string", metaValues={})
-	@JoinColumn(name="entity_id")
+	/* 
+	 * NOTE: The original mapping was:
+	 * 
+	 * @Any(metaColumn=@Column(name="entity_table"), fetch=FetchType.LAZY) 
+	 * @AnyMetaDef(idType="long", metaType="string", metaValues={})
+	 * @JoinColumn(name="entity_id")
+	 * 
+	 * This causes an ObjectNotFoundException when the referenced entity
+	 * has been deleted, as @NotFound(NotFoundAction.IGNORE) is currently
+	 * not supported for Any-mappings. See:
+	 * http://opensource.atlassian.com/projects/hibernate/browse/HHH-3475
+	 */
+	
+	@Type(type="org.riotfamily.riot.hibernate.support.FailSafeAnyType")
+	@Columns(columns = {
+	    @Column(name="entity_table"),
+	    @Column(name="entity_id")
+	})
 	@Cascade({CascadeType.MERGE, CascadeType.SAVE_UPDATE})
 	public Object getValue() {
 		return value;
