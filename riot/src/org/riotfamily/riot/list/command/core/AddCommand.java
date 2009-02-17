77ee6fd4a735736c6ae422b5781a88244609951a
Felix Gnass <felix.gnass@riotfamily.org>
2009-02-17 14:09:33 +0000

    Fixed the AddCommand so that it doesn't behave like "edit" when an item is selected.

--- riot/src/org/riotfamily/riot/list/command/core/AddCommand.java
+++ riot/src/org/riotfamily/riot/list/command/core/AddCommand.java
@@ -23,12 +23,34 @@
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.riot.list.command.core;
 
-public class AddCommand extends EditCommand {
+import org.riotfamily.riot.editor.EditorDefinition;
+import org.riotfamily.riot.editor.IntermediateDefinition;
+import org.riotfamily.riot.editor.ListDefinition;
+import org.riotfamily.riot.list.command.CommandContext;
+import org.riotfamily.riot.list.command.CommandResult;
+import org.riotfamily.riot.list.command.result.GotoUrlResult;
+import org.springframework.util.Assert;
+
+public class AddCommand extends AbstractCommand {
 	
-public static final String ACTION_ADD = "add";
+	public static final String ACTION_ADD = "add";
 	
 	public String getAction() {
 		return ACTION_ADD;
+	}
+		
+	public CommandResult execute(CommandContext context) {
+		EditorDefinition def = context.getListDefinition().getDisplayDefinition();
+		Assert.notNull(def, "A DisplayDefinition must be set in order to use the EditCommand.");
+		String editorUrl;
+		if (def instanceof IntermediateDefinition) {
+			ListDefinition listDef = ((IntermediateDefinition) def).getNestedListDefinition();
+			editorUrl = listDef.getEditorUrl(null, null, context.getListDefinition().getId());
+		}
+		else {
+			editorUrl = def.getEditorUrl(null,	context.getParentId(), context.getParentEditorId());
+		}
+		return new GotoUrlResult(context, editorUrl);
 	}
 	
 }
