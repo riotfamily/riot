6bf0f8f61c844955f566e67fd06fb8508c57e8dc
Felix Gnass <felix.gnass@riotfamily.org>
2009-04-01 15:10:46 +0000

    Removed the FormScreenSupport class

--- core/src/org/riotfamily/core/screen/form/FormScreen.java
+++ core/src/org/riotfamily/core/screen/form/FormScreen.java
@@ -23,17 +23,47 @@
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.core.screen.form;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Collection;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.riotfamily.common.util.ResourceUtils;
+import org.riotfamily.common.web.util.ServletUtils;
+import org.riotfamily.core.dao.InvalidPropertyValueException;
+import org.riotfamily.core.dao.RiotDao;
+import org.riotfamily.core.dao.RiotDaoException;
 import org.riotfamily.core.screen.RiotScreen;
+import org.riotfamily.core.screen.ScreenContext;
 import org.riotfamily.core.screen.ScreenUtils;
+import org.riotfamily.forms.Form;
+import org.riotfamily.forms.controller.AjaxFormController;
 import org.riotfamily.forms.controller.FormContextFactory;
 import org.riotfamily.forms.factory.FormRepository;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
-public class FormScreen extends FormScreenSupport implements BeanNameAware {
+public class FormScreen extends AjaxFormController
+		implements RiotScreen, BeanNameAware {
+
+	private static final DefaultTransactionDefinition TX_DEF =
+			new DefaultTransactionDefinition(
+			TransactionDefinition.PROPAGATION_REQUIRED);
+	
+	private PlatformTransactionManager transactionManager;
+
+	private FormRepository formRepository;
+	
+	private String viewName = ResourceUtils.getPath(
+			FormScreen.class, "form.ftl");
 
 	private String id;
 	
@@ -43,12 +73,17 @@ public class FormScreen extends FormScreenSupport implements BeanNameAware {
 	
 	private List<RiotScreen> childScreens;
 	
-
 	public FormScreen(FormContextFactory formContextFactory,
 			FormRepository formRepository,
 			PlatformTransactionManager transactionManager) {
 		
-		super(formContextFactory, formRepository, transactionManager);
+		super(formContextFactory);
+		this.formRepository = formRepository;
+		this.transactionManager = transactionManager;
+	}
+
+	public void setViewName(String viewName) {
+		this.viewName = viewName;
 	}
 
 	public String getFormId() {
@@ -75,6 +110,132 @@ public class FormScreen extends FormScreenSupport implements BeanNameAware {
 		}
 	}
 	
+	/**
+	 * Returns the name of the attribute under which the {@link Form} is
+	 * stored in the HTTP session. This implementation returns the
+	 * requestURI with the controller's class name as prefix. 
+	 */
+	@Override
+	protected String getSessionAttribute(HttpServletRequest request) {
+		return getClass().getName() + ":" + request.getRequestURI();
+	}
+		
+	@Override
+	protected Form createForm(HttpServletRequest request) {
+		Form form = formRepository.createForm(getFormId());
+		form.addButton("save");
+		return form;
+	}
+
+	@Override
+	protected Object getFormBackingObject(HttpServletRequest request) {
+		return ScreenContext.get(request).getObject();
+	}
+	
+	@Override
+	protected ModelAndView showForm(Form form, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		StringWriter sw = new StringWriter();
+		renderForm(form, new PrintWriter(sw));
+		ModelAndView mv = new ModelAndView(viewName);
+		mv.addObject("form", sw.toString());
+		return mv;
+	}
+
+	@Override
+	public final ModelAndView handleFormSubmission(Form form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		ScreenContext context = ScreenContext.get(request);
+		try {
+			return handleFormSubmissionInternal(form, request, response, context);
+		}
+		catch (InvalidPropertyValueException e) {
+			form.getErrors().rejectValue(e.getField(), e.getCode(),
+					e.getArguments(), e.getMessage());
+
+			return showForm(form, request, response);
+		}
+		catch (RiotDaoException e) {
+			form.getErrors().reject(e.getCode(), e.getArguments(), e.getMessage());
+			return showForm(form, request, response);
+		}
+	}
+
+	protected ModelAndView handleFormSubmissionInternal(Form form, 
+			HttpServletRequest request, HttpServletResponse response,
+			ScreenContext context) throws Exception {
+		
+		boolean save = form.isNew();
+		saveOrUpdate(form, context);
+		removeFormFromSession(request);
+		return afterSaveOrUpdate(form, request, context, save);
+	}
+		
+	protected void saveOrUpdate(Form form, ScreenContext context) 
+			throws Exception {
+		
+		TransactionStatus status = transactionManager.getTransaction(TX_DEF);
+		RiotDao dao = context.getDao();
+		try {
+			if (form.isNew()) {
+				log.debug("Saving entity ...");
+				Object parent = context.getParent();
+				Object bean = form.populateBackingObject();				
+				dao.save(bean, parent);
+				context.setObject(bean);
+			}
+			else {
+				log.debug("Updating entity ...");
+				Object bean = form.getBackingObject();
+				Object merged = dao.merge(bean);
+				form.setBackingObject(merged);
+				form.populateBackingObject();
+				dao.update(merged);
+			}
+		}
+		catch (Exception e) {
+			transactionManager.rollback(status);
+			throw e;
+		}
+		transactionManager.commit(status);
+	}
+
+	private boolean hasChildScreens() {
+		return getChildScreens() != null && !getChildScreens().isEmpty();
+	}
+	
+	protected ModelAndView afterSaveOrUpdate(
+			Form form, HttpServletRequest request,
+			ScreenContext context, boolean save) {
+		
+		String focus = request.getParameter("focus");
+		if (focus != null || (save && hasChildScreens())) {
+			return reloadForm(form, context, focus);
+		}
+		else {
+			return showParentList(context);
+		}
+	}
+
+	protected ModelAndView showParentList(ScreenContext context) {
+		String listUrl = context.createParentContext().getUrl();
+		return new ModelAndView(new RedirectView(listUrl, true));
+	}
+
+	protected ModelAndView reloadForm(Form form, ScreenContext context,
+			String focusElement) {
+
+		String formUrl = context.getUrl();
+		formUrl = ServletUtils.addParameter(formUrl, "saved", "true");
+		if (focusElement != null) {
+			formUrl = ServletUtils.addParameter(formUrl, "focus", focusElement);
+		}
+		return new ModelAndView(new RedirectView(formUrl, true));
+	}
+
 	// -----------------------------------------------------------------------
 	// Implementation of the RiotScreen interface
 	// -----------------------------------------------------------------------
