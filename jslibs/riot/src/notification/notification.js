9f94372daa1c53e20306e42a93c14d8e0ecc1e20
Felix Gnass <felix.gnass@riotfamily.org>
2009-04-07 16:20:22 +0000

    Added image preloading and autoHide feature

--- jslibs/riot/src/notification/notification.js
+++ jslibs/riot/src/notification/notification.js
@@ -38,30 +38,71 @@ riot.notification = (function() {
 		  </tbody>\
 		</table>');
 
+	var Notification = Class.create({
+		initialize: function(o) {
+			o = Object.extend({
+				autoHide: true,
+				duration: 4
+			}, o);
+			
+			var data = {
+				title: o.title, 
+				message: o.message
+			};
+			this.el = new Element('div', {className: 'notification'}).setStyle({display: 'none'})
+				.insert(template.evaluate(data))
+				.observe('mouseover', this.mouseover.bind(this))
+				.observe('mouseout', this.mouseout.bind(this));
+			
+			this.el.down('.t .l').observe('click', this.close.bind(this));
+		
+			if (o.autoHide) {
+				setTimeout(this.scheduledClose.bind(this), o.duration * 1000);
+			}
+			Element.insert(document.body, this.el);
+			Effect.Appear(this.el, {duration: 0.8});
+		},
+		
+		close: function() {
+			if (!this.closed) {
+				this.el.remove();
+				this.closed = true;
+			}
+		},
+		
+		scheduledClose: function() {
+			if (!this.hover) {
+				this.close();
+			}
+			else {
+				this.closeOnOut = true;
+			}
+		},
+		
+		mouseover: function() {
+			this.hover = true;
+			this.el.down().addClassName('hi');
+		},
+		
+		mouseout: function() {
+			this.hover = false;
+			if (this.closeOnOut) {
+				setTimeout(this.scheduledClose.bind(this), 1000);
+			}
+			else {
+				this.el.down().removeClassName('hi');
+			}
+		}
+	});
+	
 	// ------------------------------------------------------------------------
 	// Public API
 	// ------------------------------------------------------------------------
 
 	return {
-		show: function(o) {
-			var data = {
-				title: o.title, 
-				message: o.message
-			};
-			var el = new Element('div', {className: 'notification'}).setStyle({display: 'none'})
-				.insert(template.evaluate(data))
-				.observe('mouseover', function() {
-					this.down().addClassName('hi');
-				})
-				.observe('mouseout', function() {
-					this.down().removeClassName('hi');
-				})
-				.observe('click', function() {
-					this.remove();
-				});
-
-			Element.insert(document.body, el);
-			Effect.Appear(el);
+		
+		show: function(settings) {
+			new Notification(settings);
 		}
 	};
 
