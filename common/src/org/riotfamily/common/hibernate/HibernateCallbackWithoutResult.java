a91a0f8857e60fe2f1296fa1cb5bf207be822092
Felix Gnass <felix.gnass@riotfamily.org>
2009-05-18 09:47:52 +0000

    Replaced the LongConversationCallback class with ThreadBoundHibernateTemplate

--- common/src/org/riotfamily/common/hibernate/HibernateCallbackWithoutResult.java
+++ common/src/org/riotfamily/common/hibernate/HibernateCallbackWithoutResult.java
@@ -18,15 +18,47 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Felix Gnass [fgnass at neteye dot de]
+ *   flx
  *
  * ***** END LICENSE BLOCK ***** */
 package org.riotfamily.common.hibernate;
 
+import java.sql.SQLException;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
 
-public interface LongConversationCallback {
+/**
+ * Convenient base class for callbacks that don't return a result. It also
+ * allows the callback code to throw checked exceptions which are automatically
+ * wrapped into a {@link RuntimeException}.
+ * 
+ * @since 9.0
+ */
+public abstract class HibernateCallbackWithoutResult implements HibernateCallback {
 
-	public void doInHibernate(Session session) throws Exception;
+	public final Object doInHibernate(Session session) 
+			throws HibernateException, SQLException {
+		
+		try {
+			doWithoutResult(session);
+		}
+		catch (HibernateException e) {
+			throw e;
+		}
+		catch (SQLException e) {
+			throw e;
+		}
+		catch (RuntimeException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+
+	protected abstract void doWithoutResult(Session session) throws Exception;
 
 }
---
+++ common/src/org/riotfamily/common/hibernate/ThreadBoundHibernateTemplate.java
@@ -0,0 +1,129 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Riot.
+ *
+ * The Initial Developer of the Original Code is
+ * Neteye GmbH.
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   flx
+ *
+ * ***** END LICENSE BLOCK ***** */
+package org.riotfamily.common.hibernate;
+
+import java.sql.SQLException;
+
+import org.hibernate.FlushMode;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.riotfamily.common.util.RiotLog;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.springframework.orm.hibernate3.SessionHolder;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.util.Assert;
+
+/**
+ * Subclass of {@link HibernateTemplate} that binds the Hibernate Session to
+ * the current thread.
+ */
+public class ThreadBoundHibernateTemplate extends HibernateTemplate {
+
+	private RiotLog log = RiotLog.get(ThreadBoundHibernateTemplate.class);
+	
+	public ThreadBoundHibernateTemplate() {
+		super();
+	}
+
+	public ThreadBoundHibernateTemplate(SessionFactory sessionFactory, boolean allowCreate) {
+		super(sessionFactory, allowCreate);
+	}
+
+	public ThreadBoundHibernateTemplate(SessionFactory sessionFactory) {
+		super(sessionFactory);
+	}
+
+	@Override
+	protected Object doExecute(HibernateCallback action, boolean enforceNewSession, boolean enforceNativeSession)
+			throws DataAccessException {
+
+		Assert.notNull(action, "Callback object must not be null");
+
+		Session oldSession = null;
+		Session session = (enforceNewSession ?
+				SessionFactoryUtils.getNewSession(getSessionFactory(), getEntityInterceptor()) 
+				: getSession());
+		
+		SessionHolder sessionHolder = (SessionHolder) TransactionSynchronizationManager.getResource(getSessionFactory());
+
+		boolean existingTransaction = !enforceNewSession &&
+				(!isAllowCreate() || (sessionHolder != null && sessionHolder.containsSession(session)));
+		
+		if (existingTransaction) {
+			log.debug("Found thread-bound Session for LongConversationTemplate");
+		}
+		else {
+			if (sessionHolder != null) {
+				oldSession = sessionHolder.getSession();
+				sessionHolder.addSession(session);
+			}
+			else {
+				TransactionSynchronizationManager.bindResource(getSessionFactory(), new SessionHolder(session));
+			}
+		}
+
+		FlushMode previousFlushMode = null;
+		try {
+			previousFlushMode = applyFlushMode(session, existingTransaction);
+			enableFilters(session);
+			Session sessionToExpose = (enforceNativeSession || isExposeNativeSession() ? session : createSessionProxy(session));
+			Object result = action.doInHibernate(sessionToExpose);
+			flushIfNecessary(session, existingTransaction);
+			return result;
+		}
+		catch (HibernateException ex) {
+			throw convertHibernateAccessException(ex);
+		}
+		catch (SQLException ex) {
+			throw convertJdbcAccessException(ex);
+		}
+		catch (RuntimeException ex) {
+			// Callback code threw application exception...
+			throw ex;
+		}
+		finally {
+			if (existingTransaction) {
+				log.debug("Not closing pre-bound Hibernate Session after HibernateTemplate");
+				disableFilters(session);
+				if (previousFlushMode != null) {
+					session.setFlushMode(previousFlushMode);
+				}
+			}
+			else {
+				SessionFactoryUtils.closeSession(session);
+				if (oldSession != null) {
+					sessionHolder.addSession(oldSession);
+				}
+				else {
+					TransactionSynchronizationManager.unbindResource(getSessionFactory());
+				}
+			}
+		}
+	}
+	
+}
