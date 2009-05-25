8b5e1e6fa122b6da574ce99ec65d5a01f9698cae
Felix Gnass <felix.gnass@riotfamily.org>
2009-05-25 10:25:54 +0000

    Advanced syntax to include parts of the placeholder in the property-keys.

--- common/src/org/riotfamily/common/beans/config/PropertiesPlaceholderConfigurer.java
+++ common/src/org/riotfamily/common/beans/config/PropertiesPlaceholderConfigurer.java
@@ -35,26 +35,26 @@ import org.springframework.web.context.support.ServletContextPropertyPlaceholder
  * <p>
  * Example:
  * <pre>
- * &lt;bean class="org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer"&gt;
- *   &lt;property name="freemarkerSettings" ref="${freemarker.*}" /&gt;
+ * &lt;bean class="org.riotfamily.common.hibernate.RiotSessionFactoryBean"&gt;
+ *   &lt;property name="hibernateProperties" value="${riot.(hibernate.*)}" /&gt;
  * &lt;/bean&gt;
  * </pre>
  *
  * The configurer will look for all properties start start with 
- * '<code>freemarker.</code>'. So having a properties file like this ...
+ * '<code>riot.hibernate.</code>'. So having a properties file like this ...
  * 
  * <pre>
- * freemarker.number_format = 0.######
- * freemarker.locale = en_US
+ * riot.hibernate.cache.use_query_cache = true
+ * riot.hibernate.show_sql = false
  * </pre>
  * 
  * ... would be equivalent to writing:
  * <pre>
- * &lt;bean class="org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer"&gt;
- *   &lt;property name="freemarkerSettings"&gt;
+ * &lt;bean class="org.riotfamily.common.hibernate.RiotSessionFactoryBean"&gt;
+ *   &lt;property name="hibernateProperties"&gt;
  *     &lt;value&gt;
- *       number_format = 0.######
- *       locale = en_US
+ *       hibernate.cache.use_query_cache = true
+ *       hibernate.show_sql = false
  *     &lt/value&gt;
  *   &lt;/property&gt;
  * &lt;/bean&gt;
@@ -63,6 +63,13 @@ import org.springframework.web.context.support.ServletContextPropertyPlaceholder
  * Spring's {@link PropertiesEditor} will then take care of converting the 
  * String value into a <code>java.util.Properties</code> object.
  * 
+ * <p>
+ * Since Riot 9.0, the placeholder may contain parenthesis to indicate that 
+ * a part of the placeholder should be included in the resulting keys. 
+ * For example the placeholder <code>${riot.(hibernate.*)}</code> will result 
+ * in keys that all start with <code>"hibernate."</code>. If the parenthesis 
+ * were omitted, only the part matched by the asterisk would be used as key.
+ *   
  * @author Felix Gnass [fgnass at neteye dot de]
  * @since 6.4
  */
@@ -77,13 +84,26 @@ public class PropertiesPlaceholderConfigurer extends
 		return super.resolvePlaceholder(placeholder, props);
 	}
 	
-	protected String resolveAll(Properties props, String prefix) {
+	protected String resolveAll(Properties props, String match) {
+		String prefix = match;
+		int i = match.indexOf('(');
+		if (i != -1) {
+			if (i > match.length()-1) {
+				prefix = match.substring(i + 1);
+			}
+			else {
+				prefix = "";
+			}
+			match = match.substring(0, i) + prefix;
+		}
+		
 		StringBuffer sb = new StringBuffer();
 		Enumeration<?> names = props.propertyNames();
 		while (names.hasMoreElements()) {
 			String name = (String) names.nextElement();
-			if (name.startsWith(prefix) && name.length() > prefix.length()) {
-				sb.append(name.substring(prefix.length()));
+			if (name.startsWith(match) && name.length() > match.length()) {
+				sb.append(prefix);
+				sb.append(name.substring(match.length()));
 				sb.append('=');
 				sb.append(props.getProperty(name));
 				sb.append('\n');
