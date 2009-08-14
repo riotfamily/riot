5781688cd2861ba9c779a8ef137bb5fe94bed937
Carsten Woelk <carsten.woelk@riotfamily.org>
2009-08-14 14:54:39 +0200

    Missed file for commit 32983c0b5cc52a3235f7906ea0e8d8577607f6df: Fixed linkChecker to skip some links

--- linkcheck/src/org/riotfamily/linkcheck/LinkCheckLinkFilter.java
+++ linkcheck/src/org/riotfamily/linkcheck/LinkCheckLinkFilter.java
@@ -1,21 +1,21 @@
 package org.riotfamily.linkcheck;
 
 import org.riotfamily.common.web.util.ServletUtils;
-import org.riotfamily.crawler.DefaultLinkExtractor;
+import org.riotfamily.crawler.LinkFilter;
 
-public class LinkCheckLinkExtractor extends DefaultLinkExtractor {
+public class LinkCheckLinkFilter implements LinkFilter {
 
 	private static final String[] IGNORED_SCHEMES = { 
 		"javascript:", "mailto:", "ftp:" 
 	};
 	
-	protected boolean accept(String base, String uri) {
-		return !ServletUtils.isAbsoluteUrl(uri) || !ignoreScheme(uri); 
+	public boolean accept(String base, String href) {
+		return !ServletUtils.isAbsoluteUrl(href) || !ignoreScheme(href); 
 	}
 	
-	protected boolean ignoreScheme(String uri) {
+	protected boolean ignoreScheme(String href) {
 		for (int i = 0; i < IGNORED_SCHEMES.length; i++) {
-			if (uri.startsWith(IGNORED_SCHEMES[i])) {
+			if (href.startsWith(IGNORED_SCHEMES[i])) {
 				return true;
 			}
 		}
