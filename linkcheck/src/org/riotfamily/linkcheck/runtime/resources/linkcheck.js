adf329b58eb27697967a6fc729255ac89ede2cdb
Felix Gnass <flx@sark.eye>
2009-09-14 12:31:55 +0200

    Removed the linkcheck module

--- linkcheck/src/org/riotfamily/linkcheck/runtime/resources/linkcheck.js
+++
@@ -1,9 +0,0 @@
-Resources.loadScriptSequence([
-	{src: 'dwr/engine.js', test: 'dwr.engine'},
-	{src: 'dwr/util.js', test: 'dwr.util'},
-	{src: 'dwr/interface/BrokenLinkService.js', test: 'BrokenLinkService'},
-	{src: 'riot-js/util.js'},
-	{src: 'brokenlinks.js'}
-]);
-
-Resources.loadStyleSheet('style/linkcheck.css');
