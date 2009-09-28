96d56d7f95d6be709ae928d64204707d3bb02c37
Felix Gnass <flx@sark.eye>
2009-09-28 19:12:32 +0200

    Moved classes to the form package

--- content/src/org/riotfamily/components/riot/form/AbstractFrontOfficeFormController.java
+++ content/src/org/riotfamily/components/riot/form/AbstractFrontOfficeFormController.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.riotfamily.components.editor;
+package org.riotfamily.components.riot.form;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
