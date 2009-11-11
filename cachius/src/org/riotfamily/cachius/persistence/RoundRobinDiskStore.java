e4bd07edc2c09df34f8b31e18a301d571b2243cb
Felix Gnass <felix.gnass@riotfamily.org>
2009-11-11 12:54:13 +0100

    Added a RoundRobinDiskStore implementation

---
+++ cachius/src/org/riotfamily/cachius/persistence/RoundRobinDiskStore.java
@@ -0,0 +1,69 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.riotfamily.cachius.persistence;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RoundRobinDiskStore implements DiskStore {
+
+	private File baseDir;
+	
+	private int depth;
+	
+	private int[] index;
+	
+	private int maxFilesPerDir = 500;
+	
+	public RoundRobinDiskStore(File baseDir, int depth) {
+		this.baseDir = baseDir;
+		this.depth = depth;
+		this.index = new int[depth];
+	}
+	
+	public void setMaxFilesPerDir(int maxFilesPerDir) {
+		this.maxFilesPerDir = maxFilesPerDir;
+	}
+
+	public synchronized File getFile() throws IOException {
+		File f = null;
+		while (f == null) {
+			f = createFile();
+		}
+		return f;
+	}
+	
+	private File createFile() throws IOException {
+		File f = getFile(depth-1);
+		index[depth-1]++;
+		f.getParentFile().mkdirs();
+		if (f.createNewFile()) {
+			return f;
+		}
+		return null;
+	}
+	
+	private File getFile(int level) {
+		if (level == 0) {
+			return baseDir;
+		}
+		if (index[level] >= maxFilesPerDir) {
+			index[level] = 0;
+			if (level > 1) {
+				index[level-1]++;
+			}
+		}
+		return new File(getFile(level-1), String.format("%03d", index[level]));
+	}
+
+}
---
+++ cachius/test/org/riotfamily/cachius/persistence/RoundRobinDiskStoreTest.java
@@ -0,0 +1,57 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.riotfamily.cachius.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RoundRobinDiskStoreTest {
+
+	private File baseDir;
+	
+	@Before
+    public void setUp() {
+		baseDir = new File(System.getProperty("java.io.tmpdir"), "test");
+		delete(baseDir);
+		baseDir.mkdirs();
+    }
+	
+	private static void delete(File f) {
+        if (f.isDirectory()) {
+            File[] entries = f.listFiles();
+            for (int i = 0; i < entries.length; i++) {
+            	delete(entries[i]);
+            }
+        }
+        f.delete();
+    }
+	
+	@Test
+	public void testDirCreation() throws IOException {
+		RoundRobinDiskStore store = new RoundRobinDiskStore(baseDir, 4);
+		store.setMaxFilesPerDir(4);
+		LinkedList<String> paths = new LinkedList<String>();
+		for (int i = 0; i < 10; i++) {
+			String path = store.getFile().getPath();
+			Assert.assertTrue(path.startsWith(baseDir.getPath()));
+			paths.add(path.substring(baseDir.getPath().length()));
+		}
+		Assert.assertEquals("/000/000/000", paths.getFirst());
+		Assert.assertEquals("/000/002/001", paths.getLast());
+	}
+}
