2e10f6a71221beed0f8548adc34352de52170f8e
Yannick Block <ybl@franz.(none)>
2009-11-12 18:30:22 +0100

    Fixed linkchecker package for Riot9

--- linkcheck/src/org/riotfamily/linkcheck/runtime/resources/brokenlinks.js
+++ linkcheck/src/org/riotfamily/linkcheck/runtime/resources/brokenlinks.js
@@ -1,9 +1,15 @@
 function markBrokenLinks(brokenLinks) {
 	$$('a[href]').each(function(link) {
 		if (brokenLinks.include(link.href)) {
-			var span  = RBuilder.node('span', {style: {position: 'relative', paddingTop: '1px'}});
-			var marker = RBuilder.node('div', {className: 'broken-link-marker', parent: span, style: {position: 'absolute', left: '0px', visibility: 'hidden'}});
-			RElement.prependChild(link, span);
+			var span  = new Element('span').setStyle({position: 'relative', paddingTop: '1px'});
+			var marker = new Element('div').addClassName('broken-link-marker').setStyle({position: 'absolute', left: '0px', visibility: 'hidden'});
+			span.insert(marker);
+			if (link.firstChild) {
+				link.insertBefore(span, link.firstChild);
+			}
+			else {
+				link.appendChild(span);
+			}
 			marker.style.top = -marker.getHeight() + 'px';
 			marker.style.visibility = 'visible';
 			Element.addClassName(link, 'broken-link');
