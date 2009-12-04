d0793256ba076cfae42048b1c711ca12be16d9db
Felix Gnass <felix.gnass@riotfamily.org>
2009-12-04 17:41:26 +0100

    Revert "ComponentListConfig JSON is now written to the DOM" as it does not work in IE when updating content via AJAX. This reverts commit 5f9b984bfe8078900b1f0c2ae2ad4f78e7ccacda.

--- content/src/org/riotfamily/components/runtime/resources/components.js
+++ content/src/org/riotfamily/components/runtime/resources/components.js
@@ -139,7 +139,7 @@ riot.components = (function() {
 			this.element = el;
 			this.id = el.readAttribute('riot:listId');
 			if (!el.id) el.id = 'riot-list-' + this.id;
-			this.config = eval('(' + el.down('script[type=application/riot-config]').innerHTML + ')');
+			this.config = window["riotComponentListConfig" + this.id];
 			this.findComponentElements();
 		},
 		
@@ -697,8 +697,6 @@ riot.components = (function() {
 	// -----------------------------------------------------------------------
 	// Initialization
 	// -----------------------------------------------------------------------
-	
-	Prototype.ScriptFragment = '<script(?![^>]*riot-config)[^>]*>([\\S\\s]*?)<\/script>';
 	
 	var containersToPublish = [];
 	
