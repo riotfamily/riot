3f4567f6bf49b6c45864a98e12ef03357629fbbb
Felix Gnass <felix.gnass@riotfamily.org>
2009-12-07 17:50:49 +0100

    Added logging if FlashModelExposer would overwrite an already present value

--- common/src/org/riotfamily/common/web/mvc/scope/FlashModelExposer.java
+++ common/src/org/riotfamily/common/web/mvc/scope/FlashModelExposer.java
@@ -13,13 +13,19 @@
 package org.riotfamily.common.web.mvc.scope;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.riotfamily.common.web.mvc.view.ModelPostProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ObjectUtils;
 
 public class FlashModelExposer implements ModelPostProcessor {
+
+	private Logger log = LoggerFactory.getLogger(FlashModelExposer.class);
 
 	public void postProcess(Map<String, Object> model,
 			HttpServletRequest request, HttpServletResponse response)
@@ -27,7 +33,24 @@ public class FlashModelExposer implements ModelPostProcessor {
 		
 		FlashModel flash = FlashModel.get(request);
 		if (flash != null) {
-			model.putAll(flash);
+			for (Entry<String, Object> entry : flash.entrySet()) {
+				if (model.containsKey(entry.getKey())) {
+					Object existingValue = model.get(entry.getKey());
+					if (ObjectUtils.nullSafeEquals(existingValue, entry.getValue())) {
+						log.info("The model already contains the same value " +
+								"that is to be exposed from FlashScope under key '{}': {}", entry.getKey(), existingValue);
+					}
+					else {
+						log.error(String.format(
+								"The model already contains a different value under the key '%s': %s -- Value in FlashScope is: %s",
+								entry.getKey(), existingValue, entry.getValue()));
+					}
+				}
+				else {
+					log.debug("Exposing {} under key '{}'", entry.getKey(), entry.getValue());
+					model.put(entry.getKey(), entry.getValue());
+				}
+			}
 		}
 	}
 
