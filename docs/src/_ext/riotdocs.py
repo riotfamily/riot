2c2f47b042b0198f2641b024ffb7dbcc24840f50
Felix Gnass <felix.gnass@riotfamily.org>
2010-02-18 17:37:36 +0100

    The apirefs dictionary is now a conf_value and may contain references to other configuration values like ${revision}

--- docs/src/_ext/riotdocs.py
+++ docs/src/_ext/riotdocs.py
@@ -8,33 +8,38 @@ from docutils import nodes
 from sphinx.highlighting import lexers
 from freemarker import FreeMarkerHtmlLexer
 
-apirefs = {
-    'riot': 'http://www.riotfamily.org/static/docs/9.1.x/api',
-    'spring': 'http://static.springsource.org/spring/docs/3.0.x/javadoc-api'
-}
-
 def setup(app):
-    app.add_role('api', api_reference_role)
+    app.add_config_value('apirefs', {}, 'env')
+    app.add_config_value('eval_conf_values', 'release html_title', 'env')
+    
+    app.connect('builder-inited', init)
     lexers['ftl'] = FreeMarkerHtmlLexer()
+        
+def init(app):
+    cfg = app.config
+    
+    def resolve_conf_values(s, cfg):
+        return re.sub('\$\{(.+?)\}', lambda m: cfg[m.group(1)], s)
 
+    for key in cfg.eval_conf_values.split():
+        cfg[key] = resolve_conf_values(cfg[key], cfg)
+        
+    def role(role, rawtext, text, lineno, inliner, options={}, content=[]):
+        m = re.search('^(.*\.(.*?))(?:#(\S*))?(?:\s(.*))?$', text)
+        (fqn, classname, method, label) = m.group(1, 2, 3, 4)
+        
+        path = fqn.replace('.', '/').replace('@', '')
+        hash = ''
+        if method:
+            hash = '#' + method
+            classname += '.' + method
+        
+        for lib, uri in cfg.apirefs.iteritems():
+            if fqn.find(lib) != -1:
+                uri = resolve_conf_values(uri, cfg)
+                ref = '%s/index.html?%s%s' % (uri, path, hash)
+                node = nodes.reference(rawtext, label or classname, refuri=ref, **options)
+                node['classes'] += ['api', lib]
+                return [node], []
     
-def api_reference_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-    
-    #m = re.search('^(?:(.+?)\s*<)?(.+\.(.+))(?:#(\S+))?\s*>?$', text)
-    #(label, fqn, classname, method) = m.group(1, 2, 3, 4)
-    
-    m = re.search('^(.*\.(.*?))(?:#(\S*))?(?:\s(.*))?$', text)
-    (fqn, classname, method, label) = m.group(1, 2, 3, 4)
-    
-    path = fqn.replace('.', '/').replace('@', '')
-    hash = ''
-    if method:
-        hash = '#' + method
-        classname += '.' + method
-    
-    for lib, uri in apirefs.iteritems():
-        if fqn.find(lib) != -1:
-            ref = '%s/index.html?%s%s' % (uri, path, hash)
-            node = nodes.reference(rawtext, label or classname, refuri=ref, **options)
-            node['classes'] += ['api', lib]
-            return [node], []
+    app.add_role('api', role)
