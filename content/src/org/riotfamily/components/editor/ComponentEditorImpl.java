f2868ee0bf1bacc57168b6d06ea39e0115875889
Carsten Woelk <carsten.woelk@riotfamily.org>
2010-10-13 15:16:38 +0200

    Fixed the front end user notifications being displayed on concurrent editing

--- content/src/org/riotfamily/components/editor/ComponentEditorImpl.java
+++ content/src/org/riotfamily/components/editor/ComponentEditorImpl.java
@@ -233,6 +233,7 @@ public class ComponentEditorImpl implements ComponentEditor,
 	
 	@RemoteMethod
 	public ToolbarState getState(Long[] containerIds) {
+		initScriptSession();
 		HttpServletRequest request = WebContextFactory.get().getHttpServletRequest();
 		ToolbarState state = new ToolbarState();
 		if (containerIds != null) {
@@ -312,26 +313,32 @@ public class ComponentEditorImpl implements ComponentEditor,
 		WebContext ctx = WebContextFactory.get();
 		return new CapturingResponseWrapper(ctx.getHttpServletResponse(), sw);
 	}
+	
+	private void initScriptSession() {
+		WebContext webContext = WebContextFactory.get();
+		HttpServletRequest request = webContext.getHttpServletRequest();
+
+		ScriptSession currentSession = webContext.getScriptSession();
+		String host = request.getServerName();
+		RiotUser user = AccessController.getCurrentUser();
+
+		currentSession.setAttribute("host", host);
+		currentSession.setAttribute("userId", user.getUserId());
+	}
 
 	private void nofifyUsers() {
-		
 		WebContext webContext = WebContextFactory.get();
 		HttpServletRequest request = webContext.getHttpServletRequest();
 
 		final ScriptSession currentSession = webContext.getScriptSession();
-		final String host = request.getServerName();
 		final RiotUser user = AccessController.getCurrentUser();
-		
-		Locale locale = RequestContextUtils.getLocale(request);
-
-		currentSession.setAttribute("host", host);
-		currentSession.setAttribute("userId", user.getUserId());
-		
+		final String host = request.getServerName();
 		String userName = "";
 		if (user.getName() != null) {
 			userName = " (" + user.getName() + ")";
 		}
 		
+		Locale locale = RequestContextUtils.getLocale(request);
 		final String message = messageSource.getMessage(
 				"components.concurrentModification", 
 				new Object[] {
@@ -344,9 +351,9 @@ public class ComponentEditorImpl implements ComponentEditor,
 		Browser.withAllSessionsFiltered(
 			new ScriptSessionFilter() {
 				public boolean match(ScriptSession session) {
-					return !user.getUserId().equals(currentSession.getAttribute("userId")) 
-							&& session.getPage().equals(currentSession.getPage())
-							&& host.equals(currentSession.getAttribute("host"));
+					return !user.getUserId().equals(session.getAttribute("userId")) 
+							&& currentSession.getPage().equals(session.getPage())
+							&& host.equals(session.getAttribute("host"));
 				}
 			}, 
 			new Runnable() {
