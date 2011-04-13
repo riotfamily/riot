riotfamily/riot

commit ea34261635e5f711307bde71f97bf7efb9f032f9

Added the possibility to pass a linkPrefix manually

common/src/org/riotfamily/common/ui/Pager.java
@@ -17,6 +17,7 @@ import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.riotfamily.common.web.support.ServletUtils;
 import org.springframework.web.util.UrlPathHelper;
 
 /**
@@ -48,8 +49,6 @@ public class Pager {
 
 	private String pageParam;
 
-	private String linkPrefix;
-	
 	private String encodingScheme = "UTF-8";
 	
 	private boolean copyParameters = true;
@@ -79,10 +78,16 @@ public class Pager {
 
 	public void initialize(HttpServletRequest request, int padding,
 			String pageParam) {
+		
+		initialize(getLinkPrefix(request), padding, pageParam);
+	}
+	
+	public void initialize(String linkPrefix, int padding,
+			String pageParam) {
 
 		this.pageParam = pageParam;
-		prepareLinkPrefix(request);
-
+		linkPrefix = ServletUtils.addParameter(linkPrefix, pageParam, "");
+		
 		int start = currentPage - padding;
 		int end = currentPage + padding;
 
@@ -130,7 +135,7 @@ public class Pager {
 		}
 	}
 
-	private void prepareLinkPrefix(HttpServletRequest request) {
+	private String getLinkPrefix(HttpServletRequest request) {
 		StringBuffer url = new StringBuffer(
 				urlPathHelper.getOriginatingRequestUri(request));
 		
@@ -149,7 +154,7 @@ public class Pager {
 		}
 		url.append(pageParam);
 		url.append('=');
-		linkPrefix = url.toString();
+		return url.toString();
 	}
 
 	protected String urlEncode(String s) {

commit e0c2706e953138e7b802e7f83c99495399380319

Fixed the pager as it broke in ea34261635e5f711307bde71f97bf7efb9f032f9

common/src/org/riotfamily/common/ui/Pager.java
@@ -47,8 +47,6 @@ public class Pager {
 
 	private boolean gapToLastPage;
 
-	private String pageParam;
-
 	private String encodingScheme = "UTF-8";
 	
 	private boolean copyParameters = true;
@@ -79,13 +77,12 @@ public class Pager {
 	public void initialize(HttpServletRequest request, int padding,
 			String pageParam) {
 		
-		initialize(getLinkPrefix(request), padding, pageParam);
+		initialize(getLinkPrefix(request, pageParam), padding, pageParam);
 	}
 	
 	public void initialize(String linkPrefix, int padding,
 			String pageParam) {
 
-		this.pageParam = pageParam;
 		linkPrefix = ServletUtils.addParameter(linkPrefix, pageParam, "");
 		
 		int start = currentPage - padding;
@@ -135,25 +132,22 @@ public class Pager {
 		}
 	}
 
-	private String getLinkPrefix(HttpServletRequest request) {
+	private String getLinkPrefix(HttpServletRequest request, String pageParam) {
 		StringBuffer url = new StringBuffer(
 				urlPathHelper.getOriginatingRequestUri(request));
 		
-		url.append('?');
 		if (copyParameters) {
 			String query = urlPathHelper.getOriginatingQueryString(request);
 			if (query != null) {
 				int i = query.indexOf(pageParam);
 				if (i != -1) {
-					url.append(query.substring(0, i));
+					query = query.substring(0, i == 0 ? 0 : i - 1);
 				}
-				else {
-					url.append(query).append('&');
+				if (query.length() > 0) {
+					url.append("?").append(query);
 				}
 			}
 		}
-		url.append(pageParam);
-		url.append('=');
 		return url.toString();
 	}
 

common/src/org/riotfamily/common/web/macro/common.ftl
@@ -435,34 +435,34 @@
   - Renders a pager.
   - @see <a href="http://www.riotfamily.org/api/latest/org/riotfamily/website/generic/view/Pager.html">Pager</a>
   -->
-<#macro pager pager prev="&lt;&lt;" next="&gt;&gt;" gap="...">
-	<#if pager.pages gt 1>
+<#macro pager pager prev="&lt;&lt;" next="&gt;&gt;" gap="&hellip;">
+	<#if pager?has_content && pager.pages gt 1>
 		<#if pager.prevPage?exists>
-			<a class="prev-page" href="${pager.prevPage.link}">${prev}</a>
+			<a class="prev-page" href="${pager.prevPage.link?html}">${prev}</a>
 		</#if>
 		
 		<#if pager.firstPage?exists>
-			<a class="page" href="${pager.firstPage.link}">1</a>
+			<a class="page" href="${pager.firstPage.link?html}">1</a>
 			<#if pager.gapToFirstPage><span class="gap">${gap}</span></#if>
 		</#if>
 		
 		<#list pager.prevPages as page>
-			<a class="page" href="${page.link}">${page.number}</a>
+			<a class="page" href="${page.link?html}">${page.number}</a>
 		</#list>
 		
 		<span class="current-page">${pager.currentPage}</span>
 		
 		<#list pager.nextPages as page>
-			<a class="page" href="${page.link}">${page.number}</a>
+			<a class="page" href="${page.link?html}">${page.number}</a>
 		</#list>
 		
 		<#if pager.lastPage?exists>
 			<#if pager.gapToLastPage><span class="gap">${gap}</span></#if>
-			<a class="page" href="${pager.lastPage.link}">${pager.lastPage.number}</a>
+			<a class="page" href="${pager.lastPage.link?html}">${pager.lastPage.number}</a>
 		</#if>
 		
 		<#if pager.nextPage?exists>
-			<a class="next-page" href="${pager.nextPage.link}">${next}</a>
+			<a class="next-page" href="${pager.nextPage.link?html}">${next}</a>
 		</#if>
 	</#if>
 </#macro>