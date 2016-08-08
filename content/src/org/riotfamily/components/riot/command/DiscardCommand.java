8c96387d32e5eca27e58da41b7dfa2eb38485db3
Jan-Frederic Linde <jan-frederic.linde@yourstruly.de>
2016-08-08 14:10:26 +0200

    Added discard command

---
+++ content/src/org/riotfamily/components/riot/command/DiscardCommand.java
@@ -0,0 +1,52 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.riotfamily.components.riot.command;
+
+import org.riotfamily.components.model.ContentContainer;
+import org.riotfamily.components.model.ContentContainerOwner;
+import org.riotfamily.core.screen.list.command.CommandContext;
+import org.riotfamily.core.screen.list.command.CommandResult;
+import org.riotfamily.core.screen.list.command.impl.support.AbstractBatchCommand;
+import org.riotfamily.core.screen.list.command.result.RefreshListResult;
+
+public class DiscardCommand extends AbstractBatchCommand<ContentContainerOwner> {
+
+	@Override
+	protected String getName() {
+		return "discard";
+	}
+	
+	@Override
+	protected String getIcon() {
+		return "arrow_undo";
+	}
+	
+	@Override
+	protected boolean isEnabled(CommandContext context, ContentContainerOwner owner, int index,
+			int selectionSize) {
+		
+		return owner.getContentContainer().isDirty();
+	}
+	
+	@Override
+	protected CommandResult execute(CommandContext context, ContentContainerOwner owner,
+			int index, int selectionSize) {
+
+		ContentContainer container = owner.getContentContainer();
+		if (container.isDirty()) {
+			container.discard();
+		}
+		String objectId = context.getScreen().getDao().getObjectId(owner);
+		return new RefreshListResult(objectId).refreshAll();
+	}
+}
